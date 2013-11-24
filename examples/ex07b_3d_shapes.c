818e79638b5ff14fdae9f6a162e596bf119f82c5
Ray <raysan5@gmail.com>
2013-11-25 00:48:15 +0100

    Replaced tabs by 4 spaces

--- examples/ex07b_3d_shapes.c
+++ examples/ex07b_3d_shapes.c
@@ -1,11 +1,11 @@
 /*******************************************************************************************
 *
-*	raylib example 07b - Draw some basic 3d shapes (cube, sphere, cylinder...)
+*   raylib example 07b - Draw some basic 3d shapes (cube, sphere, cylinder...)
 *
-*	This example has been created using raylib 1.0 (www.raylib.com)
-*	raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*	Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
+*   Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
 *
 ********************************************************************************************/
 
@@ -13,38 +13,38 @@
 
 int main()
 {
-	int screenWidth = 800;
-	int screenHeight = 450;
+    int screenWidth = 800;
+    int screenHeight = 450;
 
-	Vector3 position = { 0.0, 0.0, 0.0 };
-	
+    Vector3 position = { 0.0, 0.0, 0.0 };
+    
     // Define the camera to look into our 3d world
-	Camera camera = {{ 0.0, 10.0, 10.0 }, { 0.0, 0.0, 0.0 }, { 0.0, 1.0, 0.0 }};
-	
-	// Initialization
-	//---------------------------------------------------------
-	InitWindow(screenWidth, screenHeight, "raylib example 07b - 3d shapes");
-	
+    Camera camera = {{ 0.0, 10.0, 10.0 }, { 0.0, 0.0, 0.0 }, { 0.0, 1.0, 0.0 }};
+    
+    // Initialization
+    //---------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib example 07b - 3d shapes");
+    
     SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
     //----------------------------------------------------------
-	
+    
     // Main game loop
-    while (!WindowShouldClose())	// Detect window close button or ESC key
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-		// Update
-		//-----------------------------------------------------
-		// TODO: Update your variables here
-		//-----------------------------------------------------
-		
-		// Draw
-		//-----------------------------------------------------
-		BeginDrawing();
-		
-			ClearBackground(RAYWHITE);
-			
-			Begin3dMode(camera);
-			
-				DrawCube(position, 2, 2, 2, RED);           // Draw a cube
+        // Update
+        //-----------------------------------------------------
+        // TODO: Update your variables here
+        //-----------------------------------------------------
+        
+        // Draw
+        //-----------------------------------------------------
+        BeginDrawing();
+        
+            ClearBackground(RAYWHITE);
+            
+            Begin3dMode(camera);
+            
+                DrawCube(position, 2, 2, 2, RED);           // Draw a cube
                 DrawCubeWires(position, 2, 2, 2, MAROON);   // Draw a wired-cube
                 
                 // TODO: Draw some basic 3d shapes
@@ -58,20 +58,20 @@ void DrawSphereWires(Vector3 centerPos, float radius, Color color);
 void DrawCylinder(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color);
 void DrawCylinderWires(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color);
 */
-				DrawGrid(10.0, 1.0);        // Draw a grid
-				
-			End3dMode();
-			
-			DrawFps(10, 10);
-		
+                DrawGrid(10.0, 1.0);        // Draw a grid
+                
+            End3dMode();
+            
+            DrawFps(10, 10);
+        
         EndDrawing();
-		//-----------------------------------------------------
+        //-----------------------------------------------------
     }
 
-	// De-Initialization
-	//---------------------------------------------------------
-    CloseWindow();		// Close window and OpenGL context
-	//----------------------------------------------------------
-	
+    // De-Initialization
+    //---------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //----------------------------------------------------------
+    
     return 0;
 }
--- examples/ex08_audio.c
+++ examples/ex08_audio.c
@@ -1,11 +1,11 @@
 /*******************************************************************************************
 *
-*	raylib example 08 - Audio loading and playing
+*   raylib example 08 - Audio loading and playing
 *
-*	This example has been created using raylib 1.0 (www.raylib.com)
-*	raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*	Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
+*   Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
 *
 ********************************************************************************************/
 
@@ -13,57 +13,57 @@
 
 int main()
 {
-	int screenWidth = 800;
-	int screenHeight = 450;
-	
-	// Initialization
-	//---------------------------------------------------------
-	InitWindow(screenWidth, screenHeight, "raylib example 08 - audio loading and playing");
-	
+    int screenWidth = 800;
+    int screenHeight = 450;
+    
+    // Initialization
+    //---------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib example 08 - audio loading and playing");
+    
     InitAudioDevice();      // Initialize audio device
-	
-	Sound fx = LoadSound("resources/coin.wav");         // Load WAV audio file
-	
-	bool currentKeyState = false;
-	bool previousKeyState = currentKeyState;
-	//----------------------------------------------------------
-	
+    
+    Sound fx = LoadSound("resources/coin.wav");         // Load WAV audio file
+    
+    bool currentKeyState = false;
+    bool previousKeyState = currentKeyState;
+    //----------------------------------------------------------
+    
     // Main game loop
-    while (!WindowShouldClose())	// Detect window close button or ESC key
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-		// Update
-		//-----------------------------------------------------
+        // Update
+        //-----------------------------------------------------
         currentKeyState = IsKeyPressed(KEY_SPACE);      // Check if Space have been pressed
-	
-		if (currentKeyState != previousKeyState)
-		{
-			if (currentKeyState) PlaySound(fx);         // Play the sound!
+    
+        if (currentKeyState != previousKeyState)
+        {
+            if (currentKeyState) PlaySound(fx);         // Play the sound!
             
-			previousKeyState = currentKeyState;
-		}
-		
-		//-----------------------------------------------------
-		
-		// Draw
-		//-----------------------------------------------------
-		BeginDrawing();
-		
-			ClearBackground(RAYWHITE);
-			
-			DrawText("Press SPACE to PLAY the SOUND!", 240, 200, 20, 1, LIGHTGRAY);
-		
+            previousKeyState = currentKeyState;
+        }
+        
+        //-----------------------------------------------------
+        
+        // Draw
+        //-----------------------------------------------------
+        BeginDrawing();
+        
+            ClearBackground(RAYWHITE);
+            
+            DrawText("Press SPACE to PLAY the SOUND!", 240, 200, 20, 1, LIGHTGRAY);
+        
         EndDrawing();
-		//-----------------------------------------------------
+        //-----------------------------------------------------
     }
 
-	// De-Initialization
-	//---------------------------------------------------------
-	UnloadSound(fx);        // Unload sound data
-	
-	CloseAudioDevice();     // Close audio device
-	
-    CloseWindow();		    // Close window and OpenGL context
-	//----------------------------------------------------------
-	
+    // De-Initialization
+    //---------------------------------------------------------
+    UnloadSound(fx);        // Unload sound data
+    
+    CloseAudioDevice();     // Close audio device
+    
+    CloseWindow();            // Close window and OpenGL context
+    //----------------------------------------------------------
+    
     return 0;
 }
