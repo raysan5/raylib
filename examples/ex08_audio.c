62f298ce544e5bb842c8256db5cc520150bbb8ac
raysan5 <raysan5@gmail.com>
2013-11-28 21:16:31 +0100

    Updated

--- examples/ex08_audio.c
+++ examples/ex08_audio.c
@@ -1,11 +1,11 @@
 /*******************************************************************************************
 *
-*	raylib example 08 - Audio loading and playing
+*   raylib example 08 - Audio loading and playing
 *
-*	This example has been created using raylib 1.0 (www.raylib.com)
-*	raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*	Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
+*   Copyright (c) 2013 Ramon Santamaria (Ray San - raysan@raysanweb.com)
 *
 ********************************************************************************************/
 
@@ -13,46 +13,46 @@
 
 int main()
 {
-	// Initialization
-	//--------------------------------------------------------------------------------------
-	int screenWidth = 800;
-	int screenHeight = 450;
-	
-	InitWindow(screenWidth, screenHeight, "raylib example 08 - audio loading and playing");
-	
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+    
+    InitWindow(screenWidth, screenHeight, "raylib example 08 - audio loading and playing");
+    
     InitAudioDevice();      // Initialize audio device
-	
-	Sound fx = LoadSound("resources/coin.wav");         // Load WAV audio file
-	//--------------------------------------------------------------------------------------
-	
+    
+    Sound fx = LoadSound("resources/coin.wav");         // Load WAV audio file
+    //--------------------------------------------------------------------------------------
+    
     // Main game loop
-    while (!WindowShouldClose())	// Detect window close button or ESC key
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-		// Update
-		//----------------------------------------------------------------------------------
-		if (IsKeyPressed(KEY_SPACE)) PlaySound(fx);     // Play the sound!	
-		//----------------------------------------------------------------------------------
-		
-		// Draw
-		//----------------------------------------------------------------------------------
-		BeginDrawing();
-		
-			ClearBackground(RAYWHITE);
-			
-			DrawText("Press SPACE to PLAY the SOUND!", 240, 200, 20, LIGHTGRAY);
-		
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyPressed(KEY_SPACE)) PlaySound(fx);     // Play the sound!    
+        //----------------------------------------------------------------------------------
+        
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        
+            ClearBackground(RAYWHITE);
+            
+            DrawText("Press SPACE to PLAY the SOUND!", 240, 200, 20, LIGHTGRAY);
+        
         EndDrawing();
-		//----------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------
     }
 
-	// De-Initialization
-	//--------------------------------------------------------------------------------------
-	UnloadSound(fx);        // Unload sound data
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadSound(fx);        // Unload sound data
     
-	CloseAudioDevice();     // Close audio device
-	
+    CloseAudioDevice();     // Close audio device
+    
     CloseWindow();          // Close window and OpenGL context
-	//--------------------------------------------------------------------------------------
-	
+    //--------------------------------------------------------------------------------------
+    
     return 0;
 }
