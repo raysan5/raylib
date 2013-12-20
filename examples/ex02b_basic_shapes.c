907fb14c797c3bfcf8c9e9d46c9280c056146088
raysan5 <raysan5@gmail.com>
2013-12-20 12:50:43 +0100

    Updated examples ex02b and ex04b

--- examples/ex02b_basic_shapes.c
+++ examples/ex02b_basic_shapes.c
@@ -35,31 +35,28 @@ int main()
         
             ClearBackground(RAYWHITE);
             
-            // TODO: draw some shapes... with names... :P
-/*            
-void DrawPixel(int posX, int posY, Color color);
-void DrawPixelV(Vector2 position, Color color);
-void DrawLine(int startPosX, int startPosY, int endPosX, int endPosY, Color color);
-void DrawLineV(Vector2 startPos, Vector2 endPos, Color color);
-void DrawCircle(int centerX, int centerY, float radius, Color color);
-void DrawCircleGradient(int centerX, int centerY, float radius, Color color1, Color color2);
-void DrawCircleV(Vector2 center, float radius, Color color);
-void DrawCircleLines(int centerX, int centerY, float radius, Color color);
-void DrawRectangle(int posX, int posY, int width, int height, Color color);
-void DrawRectangleRec(Rectangle rec, Color color);
-void DrawRectangleGradient(int posX, int posY, int width, int height, Color color1, Color color2);
-void DrawRectangleV(Vector2 position, Vector2 size, Color color);
-void DrawRectangleLines(int posX, int posY, int width, int height, Color color);
-void DrawTriangle(Vector2 v1, Vector2 v2, Vector2 v3, Color color);
-void DrawTriangleLines(Vector2 v1, Vector2 v2, Vector2 v3, Color color);
-void DrawPoly(Vector2 *points, int numPoints, Color color);
-void DrawPolyLine(Vector2 *points, int numPoints, Color color);
-*/            
-            DrawRectangle(screenWidth/4 - 50, screenHeight/2 - 100, 100, 100, GOLD);
-            DrawCircle(3*screenWidth/4, screenHeight/2 - 50, 50, MAROON);
-            
-            DrawText("_____", 320, 280, 50, 1, BLACK);
-        
+            DrawText("some basic shapes available on raylib", 20, 20, 20, DARKGRAY);
+
+            DrawLine(18, 42, screenWidth - 18, 42, BLACK);
+
+            DrawCircle(screenWidth/4, 120, 35, DARKBLUE);
+            DrawCircleGradient(screenWidth/4, 220, 60, GREEN, SKYBLUE);
+            DrawCircleLines(screenWidth/4, 340, 80, DARKBLUE);
+
+            DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
+            DrawRectangleGradient(screenWidth/4*2 - 90, 170, 180, 130, MAROON, GOLD);
+            DrawRectangleLines(screenWidth/4*2 - 40, 320, 80, 60, ORANGE);
+
+            DrawTriangle((Vector2){screenWidth/4*3, 80}, 
+                         (Vector2){screenWidth/4*3 - 60, 150}, 
+                         (Vector2){screenWidth/4*3 + 60, 150}, VIOLET);
+                         
+            DrawTriangleLines((Vector2){screenWidth/4*3, 160}, 
+                              (Vector2){screenWidth/4*3 - 20, 230}, 
+                              (Vector2){screenWidth/4*3 + 20, 230}, DARKBLUE);
+                              
+            DrawPoly((Vector2){screenWidth/4*3, 320}, 6, 80, 0, BROWN);
+                    
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
--- examples/ex04b_texture_rectangle.c
+++ examples/ex04b_texture_rectangle.c
@@ -17,11 +17,22 @@ int main()
     //--------------------------------------------------------------------------------------
     int screenWidth = 800;
     int screenHeight = 450;
+    
+    const char textLine1[] = "Lena image is a standard test image which has been in use since 1973.";
+    const char textLine2[] = "It comprises 512x512 pixels, and was originally cropped from the centerfold";
+    const char textLine3[] = "of November 1972 issue of Playboy magazine. The image is probably the most";
+    const char textLine4[] = "widely used test image for all sorts of image processing algorithms.";
 
     InitWindow(screenWidth, screenHeight, "raylib example 04b - texture rectangle");
     
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    Texture2D texture = LoadTexture("resources/raylib_logo.png");        // Texture loading
+    Texture2D texture = LoadTexture("resources/lena.png");        // Texture loading
+    
+    Color halfTrans = WHITE;
+    halfTrans.a = 30;
+    
+    Rectangle eyesRec = { 225, 240, 155, 50 };
+    Vector2 position = { 369, 241 };
     //--------------------------------------------------------------------------------------
     
     // Main game loop
@@ -38,11 +49,17 @@ int main()
         
             ClearBackground(RAYWHITE);
             
-            // TODO: Comming soon...
-            // TIP: Use DrawTextureRec() function
-/*
-void DrawTextureRec(Texture2D texture, Rectangle sourceRec, Vector2 position, float scale, Color tint);        
-*/        
+            DrawText("LENA", 220, 100, 20, PINK);
+            
+            DrawTexture(texture, screenWidth/2 - 256, 0, halfTrans); // Draw background image
+            
+            DrawTextureRec(texture, eyesRec, position, WHITE);  // Draw eyes part of image
+
+            DrawText(textLine1, 220, 140, 10, DARKGRAY);
+            DrawText(textLine2, 220, 160, 10, DARKGRAY);  
+            DrawText(textLine3, 220, 180, 10, DARKGRAY);  
+            DrawText(textLine4, 220, 200, 10, DARKGRAY);  
+       
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
