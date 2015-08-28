ea45223f1ff0f0f3ebe7535548829681d2a7fa7d
raysan5 <raysan5@gmail.com>
2015-08-28 18:07:39 +0200

    New examples added

--- examples/core_3d_camera_free.c
+++ examples/core_3d_camera_free.c
@@ -5,7 +5,7 @@
 *   This example has been created using raylib 1.3 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
