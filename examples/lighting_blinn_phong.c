b11cda959da6c8cbd3be4bd06273fa2e3110dff1
Ray <raysan5@gmail.com>
2015-12-30 13:46:01 +0100

    Update lighting_blinn_phong.c

--- examples/lighting_blinn_phong.c
+++ examples/lighting_blinn_phong.c
@@ -2,20 +2,10 @@
 *
 *   raylib [lighting] example - Basic Phong lighting
 *
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
 *   This example has been created using raylib 1.3 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
