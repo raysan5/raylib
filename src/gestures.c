94c92a58a1a8131c4d71ba32f18e836f6178231c
raysan5 <raysan5@gmail.com>
2016-02-13 17:08:09 +0100

    Some tweaks

--- src/gestures.c
+++ src/gestures.c
@@ -2,9 +2,9 @@
 *
 *   raylib Gestures System - Gestures Processing based on input gesture events (touch/mouse)
 *
-*   Reviewed by Ramon Santamaria
-*   Redesigned by Albert Martos and Ian Eito
 *   Initial design by Marc Palau
+*   Redesigned by Albert Martos and Ian Eito
+*   Reviewed by Ramon Santamaria (@raysan5)
 *
 *   This software is provided "as-is", without any express or implied warranty. In no event
 *   will the authors be held liable for any damages arising from the use of this software.
