30bb24aa6e80fe8ecc9eddc4efd008ecc89b60f4
Ray <raysan5@gmail.com>
2017-01-29 22:29:54 +0100

    Updated Gestures enum

--- src/gestures.h
+++ src/gestures.h
@@ -65,17 +65,17 @@
     // Gestures type
     // NOTE: It could be used as flags to enable only some gestures
     typedef enum {
-        GESTURE_NONE        = 1,
-        GESTURE_TAP         = 2,
-        GESTURE_DOUBLETAP   = 4,
-        GESTURE_HOLD        = 8,
-        GESTURE_DRAG        = 16,
-        GESTURE_SWIPE_RIGHT = 32,
-        GESTURE_SWIPE_LEFT  = 64,
-        GESTURE_SWIPE_UP    = 128,
-        GESTURE_SWIPE_DOWN  = 256,
-        GESTURE_PINCH_IN    = 512,
-        GESTURE_PINCH_OUT   = 1024
+        GESTURE_NONE        = 0,
+        GESTURE_TAP         = 1,
+        GESTURE_DOUBLETAP   = 2,
+        GESTURE_HOLD        = 4,
+        GESTURE_DRAG        = 8,
+        GESTURE_SWIPE_RIGHT = 16,
+        GESTURE_SWIPE_LEFT  = 32,
+        GESTURE_SWIPE_UP    = 64,
+        GESTURE_SWIPE_DOWN  = 128,
+        GESTURE_PINCH_IN    = 256,
+        GESTURE_PINCH_OUT   = 512
     } Gestures;
 #endif
 
