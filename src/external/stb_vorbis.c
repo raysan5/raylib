e7f0d0eef13fee1682ce674385677d8b01c7b2de
Ray <raysan5@gmail.com>
2017-04-21 00:08:24 +0200

    Corrected issue with alloca.h on GCC

--- src/external/stb_vorbis.c
+++ src/external/stb_vorbis.c
@@ -168,7 +168,7 @@
    #include <math.h>
 
    // find definition of alloca if it's not in stdlib.h:
-   #ifdef _MSC_VER
+   #if defined(_MSC_VER) || defined(__MINGW32__)
       #include <malloc.h>
    #endif
    #if defined(__linux__) || defined(__linux) || defined(__EMSCRIPTEN__)
