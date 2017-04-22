247da006ae81ee66fe0239a1c9bbbfcb0f79aa6f
raysan5 <raysan5@gmail.com>
2017-04-22 22:35:04 +0200

    Rename parameter

--- src/rlgl.h
+++ src/rlgl.h
@@ -366,7 +366,7 @@ void rlglClose(void);                           // De-init rlgl
 void rlglDraw(void);                            // Draw VAO/VBO
 void rlglLoadExtensions(void *loader);          // Load OpenGL extensions
 
-unsigned int rlglLoadTexture(void *data, int width, int height, int textureFormat, int mipmapCount);    // Load texture in GPU
+unsigned int rlglLoadTexture(void *data, int width, int height, int format, int mipmapCount);    // Load texture in GPU
 RenderTexture2D rlglLoadRenderTexture(int width, int height);   // Load a texture to be used for rendering (fbo with color and depth attachments)
 void rlglUpdateTexture(unsigned int id, int width, int height, int format, const void *data);         // Update GPU texture with new data
 void rlglGenerateMipmaps(Texture2D *texture);                       // Generate mipmap data for selected texture
