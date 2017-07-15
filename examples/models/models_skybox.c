df6bdb7d81a4cb431e691c7c821a4dde063da95e
victorfisac <victorfisac@gmail.com>
2017-07-15 16:08:35 +0200

    Fixed examples and added screenshots

--- examples/models/models_skybox.c
+++ examples/models/models_skybox.c
@@ -24,17 +24,23 @@ int main()
     // Define the camera to look into our 3d world
     Camera camera = {{ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f };
 
+    // Load skybox model and shader
+    #define PATH_SKYBOX_VS      "resources/shaders/skybox.vs"       // Path to skybox vertex shader
+    #define PATH_SKYBOX_FS      "resources/shaders/skybox.fs"       // Path to skybox vertex shader
+    #define PATH_TEXTURES_HDR   "resources/pinetree.hdr"
+    #define LOC_CUSTOM_SKYRESOLUTION    15
+    
     Mesh cube = GenMeshCube(1.0f, 1.0f, 1.0f);
     Model skybox = LoadModelFromMesh(cube, false);
-    skybox.material.shader = LoadShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
+    skybox.material.shader = LoadShader(PATH_SKYBOX_VS, PATH_SKYBOX_FS);
 
-    Texture2D texHDR = LoadTexture("resources/pinetree.hdr");
+    Texture2D texHDR = LoadTexture(PATH_TEXTURES_HDR);
     skybox.material.maps[TEXMAP_CUBEMAP].tex = rlGenMapCubemap(texHDR, 512);
     SetShaderValuei(skybox.material.shader, GetShaderLocation(skybox.material.shader, "environmentMap"), (int[1]){ TEXMAP_CUBEMAP }, 1);
 
     // Get skybox shader locations
     int skyProjectionLoc = GetShaderLocation(skybox.material.shader, "projection");
-    int skyViewLoc = GetShaderLocation(skybox.material.shader, "view");
+    skybox.material.shader.locs[LOC_MATRIX_VIEW] = GetShaderLocation(skybox.material.shader, "view");
     
     // Then before rendering, configure the viewport to the actual screen dimensions
     Matrix proj = MatrixPerspective(60.0, (double)GetScreenWidth()/(double)GetScreenHeight(), 0.01, 1000.0);
@@ -54,7 +60,7 @@ int main()
         UpdateCamera(&camera);              // Update camera
         
         Matrix view = MatrixLookAt(camera.position, camera.target, camera.up);
-        SetShaderValueMatrix(skybox.material.shader, skyViewLoc, view);
+        SetShaderValueMatrix(skybox.material.shader, skybox.material.shader.locs[LOC_MATRIX_VIEW], view);
         //----------------------------------------------------------------------------------
 
         // Draw
