2f471414c2f830e61a200ecd956f37e45ce94e96
Martinfx <maficccc@gmail.com>
2017-11-29 00:05:39 +0100

    Added compile with cmake for FreeBSD

--- utils.cmake
+++ utils.cmake
@@ -1,9 +1,8 @@
 # All sorts of things that we need cross project
 cmake_minimum_required(VERSION 2.8.0)
 
-# Detect linux
-if(UNIX AND NOT APPLE)
-  set(LINUX TRUE)
+if(CMAKE_SYSTEM_NAME STREQUAL Linux)
+	set(LINUX TRUE)
 endif()
 
 # Linking for OS X -framework options
@@ -18,16 +17,35 @@ if(APPLE)
 
   set(LIBS_PRIVATE ${OPENGL_LIBRARY} ${OPENAL_LIBRARY} ${COCOA_LIBRARY}
                    ${IOKIT_LIBRARY} ${COREFOUNDATION_LIBRARY} ${COREVIDEO_LIBRARY})
-elseif(LINUX)
+endif()
+
+if(CMAKE_SYSTEM_NAME STREQUAL Linux)
   # Elsewhere (such as Linux), need `-lopenal -lGL`, etc...
   set(LIBS_PRIVATE
       m pthread dl
       openal
       GL
       X11 Xrandr Xinerama Xi Xxf86vm Xcursor)  # X11 stuff
-else()
-  # TODO Windows
 endif()
+
+if(CMAKE_SYSTEM_NAME STREQUAL FreeBSD)
+   find_package(OpenGL REQUIRED)
+   find_package(OpenAL REQUIRED)
+   include_directories(${OPENGL_INCLUDE_DIR} ${OPENAL_INCLUDE_DIR})
+
+   find_package(X11 REQUIRED)
+   find_library(OpenAL REQUIRED)
+   find_library(pthread NAMES pthread)
+   find_library(Xrandr NAMES Xrandr)
+   find_library(Xi NAMES Xi)
+   find_library(Xinerama NAMES Xinerama)
+   find_library(Xxf86vm NAMES Xxf86vm)
+   find_library(Xcursor NAMES Xcursor)
+
+   set(LIBS_PRIVATE m ${pthread} ${OPENAL_LIBRARY} ${X11_LIBRARIES} ${Xrandr} ${Xinerama} ${Xi} ${Xxf86vm} ${Xcursor})
+endif()
+
+# TODO Support Windows
 
 # Do the linking for executables that are meant to link raylib
 function(link_libraries_to_executable executable)
