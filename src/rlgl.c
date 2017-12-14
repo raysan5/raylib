07b522c1131faf29df49da5f0f09826cef765d4b
user <email>
2017-12-14 11:46:38 +0100

    make matrix stack work closer to old opengl implementation

--- src/rlgl.c
+++ src/rlgl.c
@@ -414,7 +414,7 @@ void rlPushMatrix(void)
     }
 
     stack[stackCounter] = *currentMatrix;
-    rlLoadIdentity();
+//    rlLoadIdentity();
     stackCounter++;
 
     if (currentMatrixMode == RL_MODELVIEW) useTempBuffer = true;
