raysan5/raylib

commit 468309d06c7ac3e3278057cfb1cfc19b332833bf

Early-exit InitWindow if InitGraphicsDevice fails
Otherwise we may run into LoadDefaultFont and crash in rlLoadTexture
Also moves InitTimer() before InitGraphicsDevice(), to allow it to be
tested even if InitWindow ultimately fails.

src/core.c
@@ -441,12 +441,13 @@ void InitWindow(int width, int height, void *data)
     uwpWindow = (EGLNativeWindowType)data;
 #endif
 
+    // Init hi-res timer
+    InitTimer();
+
     // Init graphics device (display device and OpenGL context)
     // NOTE: returns true if window and graphic device has been initialized successfully
     windowReady = InitGraphicsDevice(width, height);
-
-    // Init hi-res timer
-    InitTimer();
+    if (!windowReady) return;
 
 #if defined(SUPPORT_DEFAULT_FONT)
     // Load default font

commit b4e2f5b45c22e560bf785cf412154fc32d995e1e

Initialize the timer after the graphics device on desktop and web platforms. (#516)
This is already the order that is used for Android. It doesn't appear to
make a difference on desktop but on web using the timer before it's been
initialized (by glfwInit, inside InitGraphicsDevice) causes the a long
(and variable but often several seconds) sleep between the first and
second frame.

Fixes: 468309d ("Early-exit InitWindow if InitGraphicsDevice fails")

src/core.c
@@ -442,13 +442,13 @@ void InitWindow(int width, int height, void *data)
     uwpWindow = (EGLNativeWindowType)data;
 #endif
 
-    // Init hi-res timer
-    InitTimer();
-
     // Init graphics device (display device and OpenGL context)
     // NOTE: returns true if window and graphic device has been initialized successfully
     windowReady = InitGraphicsDevice(width, height);
     if (!windowReady) return;
+
+    // Init hi-res timer
+    InitTimer();
 
 #if defined(SUPPORT_DEFAULT_FONT)
     // Load default font