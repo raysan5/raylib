276974de05f4ab01feae89b633dcfafa720a9bf2
Ray <raysan5@gmail.com>
2018-07-02 16:39:04 +0200

    Removed function prototype
    This function was added in text module as GenImageFontAtlas()

--- src/textures.c
+++ src/textures.c
@@ -2123,18 +2123,6 @@ Image GenImageCellular(int width, int height, int tileSize)
 
     return image;
 }
-
-// Generate image: font atlas. Requires TTF as input file.
-// NOTE: Generated atlas packs characters in order and rectangles are defined with magenta borders (?)
-// NOTE: Characters info data should be allocated by user for charsCount
-Image GenImageFont(const char *fileName, int fontSize, int charsCount, int *fontChars, CharInfo *chars)
-{
-    Image image = { 0 };
-    
-    // TODO.
-    
-    return image;
-}
 #endif      // SUPPORT_IMAGE_GENERATION
 
 // Generate GPU mipmaps for a texture
