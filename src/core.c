161b18edea6649a108ef3f7aa37464688adcba07
Ray <raysan5@gmail.com>
2018-10-20 12:30:37 +0200

    Reviewed possible issue with external libs

--- src/core.c
+++ src/core.c
@@ -125,7 +125,7 @@
 #include <ctype.h>          // Required for: tolower() [Used in IsFileExtension()]
 #include <sys/stat.h>       // Required for stat() [Used in GetLastWriteTime()]
 
-#if defined(_WIN32) && defined(_MSC_VER)
+#if defined(PLATFORM_DESKTOP) && defined(_WIN32) && defined(_MSC_VER)
     #include "external/dirent.h"    // Required for: DIR, opendir(), closedir() [Used in GetDirectoryFiles()]
 #else
     #include <dirent.h>             // Required for: DIR, opendir(), closedir() [Used in GetDirectoryFiles()]
