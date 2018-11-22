ab4b0508a275e64b0181c155edb015c55461c8a6
Ray <raysan5@gmail.com>
2018-11-22 11:55:27 +0100

    Added Geany project template for Windows

---
+++ projects/Geany/raylib_compile_execute.bat
@@ -0,0 +1,20 @@
+::@echo off
+:: > Setup required Environment
+:: -------------------------------------
+set RAYLIB_DIR=C:\raylib
+set PATH=%PATH%;%RAYLIB_DIR%\mingw32\bin
+set FILE_NAME=%1
+set NAME_PART=%FILE_NAME:~0,-2%
+cd %~dp0
+:: .
+:: > Cleaning latest build
+:: ---------------------------
+cmd /c if exist %NAME_PART%.exe del /F %NAME_PART%.exe
+:: .
+:: > Compiling program
+:: --------------------------
+gcc -o %NAME_PART%.exe %FILE_NAME% %RAYLIB_DIR%\raylib.rc.data -s -O2 -I../../src -Iexternal -lraylib -lopengl32 -lgdi32 -std=c99 -Wall -mwindows
+:: .
+:: . > Executing program
+:: -------------------------
+cmd /c if exist %NAME_PART%.exe %NAME_PART%.exe
