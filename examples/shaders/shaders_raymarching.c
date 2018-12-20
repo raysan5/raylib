6ef1e1d9387192a9f6b49095dc77e3d6bfa367b7
Ray <raysan5@gmail.com>
2018-12-20 09:55:33 +0100

    new example: shaders_raymarching

---
+++ examples/shaders/shaders_raymarching.c
@@ -0,0 +1,106 @@
+/*******************************************************************************************
+*
+*   raylib [shaders] example - Raymarching shapes generation
+*
+*   NOTE: This example requires raylib OpenGL 3.3 or ES2 versions for shaders support,
+*         OpenGL 1.1 does not support shaders, recompile raylib to OpenGL 3.3 version.
+*
+*   NOTE: Shaders used in this example are #version 330 (OpenGL 3.3), to test this example
+*         on OpenGL ES 2.0 platforms (Android, Raspberry Pi, HTML5), use #version 100 shaders
+*         raylib comes with shaders ready for both versions, check raylib/shaders install folder
+*
+*   This example has been created using raylib 2.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2018 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+    
+    InitWindow(screenWidth, screenHeight, "raymarching");
+
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 2.5f, 2.5f, 3.0f };    // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.7f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 65.0f;                                // Camera field-of-view Y
+    
+    SetCameraMode(camera, CAMERA_FREE);                 // Set camera mode
+   
+    // Load raymarching shader
+    // NOTE: Defining 0 (NULL) for vertex shader forces usage of internal default vertex shader
+    Shader shader = LoadShader(0, "resources/shaders/glsl330/raymarching.fs");
+    
+    // Get shader locations for required uniforms
+    int viewEyeLoc = GetShaderLocation(shader, "viewEye");
+    int viewCenterLoc = GetShaderLocation(shader, "viewCenter");
+    int viewUpLoc = GetShaderLocation(shader, "viewUp");
+    int deltaTimeLoc = GetShaderLocation(shader, "deltaTime");
+    int runTimeLoc = GetShaderLocation(shader, "runTime");
+    int resolutionLoc = GetShaderLocation(shader, "resolution");
+    
+    float resolution[2] = { screenWidth, screenHeight };
+    SetShaderValue(shader, resolutionLoc, resolution, 2);
+    
+    float runTime = 0.0f;
+    
+    SetTargetFPS(60);                       // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())            // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);              // Update camera
+        
+        float cameraPos[3] = { camera.position.x, camera.position.y, camera.position.z };
+        float cameraTarget[3] = { camera.target.x, camera.target.y, camera.target.z };
+        float cameraUp[3] = { camera.up.x, camera.up.y, camera.up.z };
+
+        float deltaTime = GetFrameTime();  
+        runTime += deltaTime;
+        
+        // Set shader required uniform values
+        SetShaderValue(shader, viewEyeLoc, cameraPos, 3);
+        SetShaderValue(shader, viewCenterLoc, cameraTarget, 3);
+        SetShaderValue(shader, viewUpLoc, cameraUp, 3);
+        SetShaderValue(shader, deltaTimeLoc, &deltaTime, 1);
+        SetShaderValue(shader, runTimeLoc, &runTime, 1);
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            // We only draw a white full-screen rectangle,
+            // frame is generated in shader using raymarching
+            BeginShaderMode(shader);
+                DrawRectangle(0, 0, screenWidth, screenHeight, WHITE);                          
+            EndShaderMode();
+            
+            DrawText("(c) Raymarching shader by Iñigo Quilez. MIT License.", screenWidth - 280, screenHeight - 20, 10, GRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadShader(shader);           // Unload shader
+
+    CloseWindow();                  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
