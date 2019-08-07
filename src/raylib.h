raysan5/raylib

commit 2a913b6587d60dab64c3b81019990e6872a9ac75

ADDED: GetImageAlphaBorder()

src/raylib.h
@@ -1095,6 +1095,7 @@ RLAPI void UnloadTexture(Texture2D texture);                                    
 RLAPI void UnloadRenderTexture(RenderTexture2D target);                                                  // Unload render texture from GPU memory (VRAM)
 RLAPI Color *GetImageData(Image image);                                                                  // Get pixel data from image as a Color struct array
 RLAPI Vector4 *GetImageDataNormalized(Image image);                                                      // Get pixel data from image as Vector4 array (float normalized)
+RLAPI Rectangle GetImageAlphaBorder(Image image);                                                        // Get image alpha border rectangle
 RLAPI int GetPixelDataSize(int width, int height, int format);                                           // Get pixel data size in bytes (image or texture)
 RLAPI Image GetTextureData(Texture2D texture);                                                           // Get pixel data from GPU texture and return an Image
 RLAPI Image GetScreenData(void);                                                                         // Get pixel data from screen buffer and return an Image (screenshot)

src/textures.c
@@ -680,6 +680,37 @@ Vector4 *GetImageDataNormalized(Image image)
     return pixels;
 }
 
+// Get image alpha border rectangle
+Rectangle GetImageAlphaBorder(Image image)
+{
+    Color *pixels = GetImageData(image);
+
+    int xMin = 65536;   // Define a big enough number
+    int xMax = 0;
+    int yMin = 65536;
+    int yMax = 0;
+
+    for (int y = 0; y < image->height; y++)
+    {
+        for (int x = 0; x < image->width; x++)
+        {
+            if (pixels[y*image->width + x].a > (unsigned char)(threshold*255.0f))
+            {
+                if (x < xMin) xMin = x;
+                if (x > xMax) xMax = x;
+                if (y < yMin) yMin = y;
+                if (y > yMax) yMax = y;
+            }
+        }
+    }
+
+    Rectangle crop = { xMin, yMin, (xMax + 1) - xMin, (yMax + 1) - yMin };
+
+    RL_FREE(pixels);
+
+    return crop;
+}
+
 // Get pixel data size in bytes (image or texture)
 // NOTE: Size depends on pixel format
 int GetPixelDataSize(int width, int height, int format)
@@ -894,7 +925,9 @@ Image ImageFromImage(Image image, Rectangle rec)
 {
     Image result = ImageCopy(image);
     
+#if defined(SUPPORT_IMAGE_MANIPULATION)
     ImageCrop(&result, rec);
+#endif
     
     return result;
 }

commit a6d0f1a920d80a9066fbd7d630d17ef912106581

Fix build caused by GetImageAlphaBorder()
Fix build failure introduced in 2a913b6587d60dab64c3b81019990e6872a9ac75

src/raylib.h
@@ -1091,7 +1091,7 @@ RLAPI void UnloadTexture(Texture2D texture);                                    
 RLAPI void UnloadRenderTexture(RenderTexture2D target);                                                  // Unload render texture from GPU memory (VRAM)
 RLAPI Color *GetImageData(Image image);                                                                  // Get pixel data from image as a Color struct array
 RLAPI Vector4 *GetImageDataNormalized(Image image);                                                      // Get pixel data from image as Vector4 array (float normalized)
-RLAPI Rectangle GetImageAlphaBorder(Image image);                                                        // Get image alpha border rectangle
+RLAPI Rectangle GetImageAlphaBorder(Image image, float threshold);                                       // Get image alpha border rectangle
 RLAPI int GetPixelDataSize(int width, int height, int format);                                           // Get pixel data size in bytes (image or texture)
 RLAPI Image GetTextureData(Texture2D texture);                                                           // Get pixel data from GPU texture and return an Image
 RLAPI Image GetScreenData(void);                                                                         // Get pixel data from screen buffer and return an Image (screenshot)

src/textures.c
@@ -681,7 +681,7 @@ Vector4 *GetImageDataNormalized(Image image)
 }
 
 // Get image alpha border rectangle
-Rectangle GetImageAlphaBorder(Image image)
+Rectangle GetImageAlphaBorder(Image image, float threshold)
 {
     Color *pixels = GetImageData(image);
 
@@ -690,11 +690,11 @@ Rectangle GetImageAlphaBorder(Image image)
     int yMin = 65536;
     int yMax = 0;
 
-    for (int y = 0; y < image->height; y++)
+    for (int y = 0; y < image.height; y++)
     {
-        for (int x = 0; x < image->width; x++)
+        for (int x = 0; x < image.width; x++)
         {
-            if (pixels[y*image->width + x].a > (unsigned char)(threshold*255.0f))
+            if (pixels[y*image.width + x].a > (unsigned char)(threshold*255.0f))
             {
                 if (x < xMin) xMin = x;
                 if (x > xMax) xMax = x;