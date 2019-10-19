68dcb553497a74b3eee11f4cb4db800d3727409d
raysan5 <raysan5@gmail.com>
2019-10-19 17:26:45 +0200

    Corrected bug on ImageDrawRectangleLines()

--- src/textures.c
+++ src/textures.c
@@ -2002,7 +2002,7 @@ void ImageDrawRectangleLines(Image *dst, Rectangle rec, int thick, Color color)
     ImageDrawRectangle(dst, (Rectangle){ rec.x, rec.y, rec.width, thick }, color);
     ImageDrawRectangle(dst, (Rectangle){ rec.x, rec.y + thick, thick, rec.height - thick*2 }, color);
     ImageDrawRectangle(dst, (Rectangle){ rec.x + rec.width - thick, rec.y + thick, thick, rec.height - thick*2 }, color);
-    ImageDrawRectangle(dst, (Rectangle){ rec.x, rec.height - thick, rec.width, thick }, color);
+    ImageDrawRectangle(dst, (Rectangle){ rec.x, rec.y + rec.height - thick, rec.width, thick }, color);
 }
 
 // Draw text (default font) within an image (destination)
