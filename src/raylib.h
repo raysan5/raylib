a2071d2a48b3ff8f164e7495b118529c3f0d438e
Darryl Dixon <piecedigitalstudios@gmail.com>
2019-11-24 16:01:23 -0500

    revert alignments

--- src/raylib.h
+++ src/raylib.h
@@ -620,8 +620,7 @@ typedef enum {
 } GamepadNumber;
 
 // Gamepad Buttons
-typedef enum
-{
+typedef enum {
     // This is here just for error checking
     GAMEPAD_BUTTON_UNKNOWN = 0,
 
@@ -647,9 +646,9 @@ typedef enum
     GAMEPAD_BUTTON_RIGHT_TRIGGER_2,
 
     // These are buttons in the center of the gamepad
-    GAMEPAD_BUTTON_MIDDLE_LEFT,  //PS3 Select
-    GAMEPAD_BUTTON_MIDDLE,       //PS Button/XBOX Button
-    GAMEPAD_BUTTON_MIDDLE_RIGHT, //PS3 Start
+    GAMEPAD_BUTTON_MIDDLE_LEFT,     //PS3 Select
+    GAMEPAD_BUTTON_MIDDLE,          //PS Button/XBOX Button
+    GAMEPAD_BUTTON_MIDDLE_RIGHT,    //PS3 Start
 
     // These are the joystick press in buttons
     GAMEPAD_BUTTON_LEFT_THUMB,
