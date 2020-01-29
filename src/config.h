a7f0fbc5206525d39fb6354f6da7926426092fe8
Ray <raysan5@gmail.com>
2020-01-29 17:47:32 +0100

    Bump raylib version to 3.0 !!!

--- src/config.h
+++ src/config.h
@@ -25,7 +25,7 @@
 *
 **********************************************************************************************/
 
-#define RAYLIB_VERSION  "2.6-dev"
+#define RAYLIB_VERSION  "3.0"
 
 // Edit to control what features Makefile'd raylib is compiled with
 #if defined(RAYLIB_CMAKE)
--- src/core.c
+++ src/core.c
@@ -116,7 +116,7 @@
 #if !defined(EXTERNAL_CONFIG_FLAGS)
     #include "config.h"         // Defines module configuration flags
 #else
-    #define RAYLIB_VERSION  "2.6-dev"
+    #define RAYLIB_VERSION  "3.0"
 #endif
 
 #if (defined(__linux__) || defined(PLATFORM_WEB)) && _POSIX_C_SOURCE < 199309L
