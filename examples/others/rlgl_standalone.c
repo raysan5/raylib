3cd9e3896aa6d3cf04ac919f03e51f2b60502906
Ray <raysan5@gmail.com>
2020-02-04 15:43:14 +0100

    tweak

--- examples/others/rlgl_standalone.c
+++ examples/others/rlgl_standalone.c
@@ -48,8 +48,8 @@
 #define RLGL_SUPPORT_TRACELOG
 #include "rlgl.h"               // OpenGL 1.1 immediate-mode style coding
 
-#ifdef __EMSCRIPTEN__
-#define GLFW_INCLUDE_ES2
+#if defined(__EMSCRIPTEN__)
+    #define GLFW_INCLUDE_ES2
 #endif
 
 #include <GLFW/glfw3.h>         // Windows/Context and inputs management
