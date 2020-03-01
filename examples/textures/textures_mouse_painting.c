117696a8936007592f7fe7c0b7b227a805e3b74a
Ray <raysan5@gmail.com>
2020-03-02 00:09:05 +0100

    Update textures_mouse_painting.c

--- examples/textures/textures_mouse_painting.c
+++ examples/textures/textures_mouse_painting.c
@@ -166,7 +166,7 @@ int main(void)
             // Draw drawing circle for reference
             if (mousePos.y > 50) 
             {
-                if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) DrawCircleLines(mousePos.x, mousePos.y, brushSize, colors[colorSelected]);
+                if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) DrawCircleLines(mousePos.x, mousePos.y, brushSize, GRAY);
                 else DrawCircle(GetMouseX(), GetMouseY(), brushSize, colors[colorSelected]);
             }
             
