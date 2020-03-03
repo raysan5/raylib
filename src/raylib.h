7da91f4788d0fc0662ff16b24d53c7837507d5be
brankoku <48296877+brankoku@users.noreply.github.com>
2020-03-03 17:14:15 -0500

    add `LoadImageBuffer`

--- src/raylib.h
+++ src/raylib.h
@@ -1098,6 +1098,7 @@ RLAPI bool CheckCollisionPointTriangle(Vector2 point, Vector2 p1, Vector2 p2, Ve
 
 // Image/Texture2D data loading/unloading/saving functions
 RLAPI Image LoadImage(const char *fileName);                                                             // Load image from file into CPU memory (RAM)
+RLAPI Image LoadImageBuffer(const char *extension, const void *data, const unsigned int length);         // Load image from file already in memory
 RLAPI Image LoadImageEx(Color *pixels, int width, int height);                                           // Load image from Color array data (RGBA - 32bit)
 RLAPI Image LoadImagePro(void *data, int width, int height, int format);                                 // Load image from raw data with parameters
 RLAPI Image LoadImageRaw(const char *fileName, int width, int height, int format, int headerSize);       // Load image from RAW file data
