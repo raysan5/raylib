66e9ba1ee9ad89614f8aec7e34116192522f6a51
codifies <nospam@antispam.com>
2020-03-24 21:44:25 +0000

    spotlight example, each spot has own radius, mouse countrol NB glsl100 shader needs testing on "bare metal"

--- examples/shaders/resources/shaders/glsl330/spotlight.fs
+++ examples/shaders/resources/shaders/glsl330/spotlight.fs
@@ -9,36 +9,61 @@ out vec4 finalColor;
 
 // NOTE: Add here your custom variables
 
-#define     MAX_SPOTS			2
-#define		RADIUS				128
-#define		INNER				96
+#define     MAX_SPOTS			3
 
-uniform vec2 spots[MAX_SPOTS];      // Spotlight positions array
+struct Spot {
+    vec2 pos;		// window coords of spot
+    float inner;	// inner fully transparent centre radius
+    float radius;	// alpha fades out to this radius
+};
+
+uniform Spot spots[MAX_SPOTS];      // Spotlight positions array
 uniform float screenWidth;          // Width of the screen
 
 void main()
 {
-	float alpha = 0.0;
+	float alpha = 1.0;
     
 	// Get the position of the current fragment (screen coordinates!)
+	
 	vec2 pos = vec2(gl_FragCoord.x, gl_FragCoord.y);
 	
 	// Find out which spotlight is nearest
 	float d = 65000; // some high value
-	float di = 0;
+	int fi = -1; // found index
 
     for (int i = 0; i < MAX_SPOTS; i++)
     {
-		di = distance(pos, spots[i]);
-		if (d > di) d = di;
+		for (int j = 0; j < MAX_SPOTS; j++)
+		{
+			float dj = distance(pos, spots[j].pos) - spots[j].radius + spots[i].radius;
+			if (d > dj ) 
+			{
+				d = dj;
+				fi = i;
+			}
+		}
     }
     
     // d now equals distance to nearest spot...
-    if (d > RADIUS) alpha = 1.0;
-	else
-    {
-		if (d < INNER) alpha = 0.0;
-		else alpha = (d - INNER)/(RADIUS - INNER);
+    // allowing for the different radii of all spotlights
+    if (fi != -1) {
+		
+		if (d > spots[fi].radius) 
+		{
+			alpha = 1.0;
+		}
+		else
+		{
+			if (d < spots[fi].inner) 
+			{
+				alpha = 0.0;
+			}
+			else 
+			{
+				alpha = (d - spots[fi].inner) / (spots[fi].radius - spots[fi].inner);
+			}
+		}
 	}
 	
 	// Right hand side of screen is dimly lit, 
