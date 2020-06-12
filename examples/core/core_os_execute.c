3835a82c429e552936bd3df3c6048ab3ab30c979
Rabia Alhaffar <steria773@gmail.com>
2020-06-12 20:57:37 +0300

    Added Execute function example

---
+++ examples/core/core_os_execute.c
@@ -0,0 +1,16 @@
+// Written by Rabia Alhaffar in 12/June/2020
+
+#include <raylib.h>
+
+int main(void) {
+    InitWindow(0,0,"Execute");
+    Execute("mkdir made_with_example");
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Command executed successfully!!!",10,10,32,BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
