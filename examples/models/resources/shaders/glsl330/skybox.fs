488c60d13945cf1b8f51ff668c5adc7a80e3b734
raysan5 <raysan5@gmail.com>
2020-07-28 11:27:05 +0200

    Small shader fix on vec3 initialization #1298

--- examples/models/resources/shaders/glsl330/skybox.fs
+++ examples/models/resources/shaders/glsl330/skybox.fs
@@ -23,7 +23,7 @@ out vec4 finalColor;
 void main()
 {
     // Fetch color from texture map
-    vec3 color = { 0.0 };
+    vec3 color = vec3(0.0);
 
     if (vflipped) color = texture(environmentMap, vec3(fragPosition.x, -fragPosition.y, fragPosition.z)).rgb;
     else color = texture(environmentMap, fragPosition).rgb;
