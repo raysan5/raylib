09cf9624bbfe89685cb1927a65485eee97a5ff4e
Elkantor <vic03@live.fr>
2020-12-21 15:11:43 +0100

    Remove bad indents

--- src/raymath.h
+++ src/raymath.h
@@ -365,8 +365,8 @@ RMDEF _Bool Vector2SegmentIntersect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 
 
     if (xi < fminf(x1, x2) || xi > fmaxf(x1, x2)) return false;
     if (xi < fminf(x3, x4) || xi > fmaxf(x3, x4)) return false;
-	if (yi < fminf(y1, y2) || yi > fmaxf(y1, y2)) return false;
-	if (yi < fminf(y3, y4) || yi > fmaxf(y3, y4)) return false;
+    if (yi < fminf(y1, y2) || yi > fmaxf(y1, y2)) return false;
+    if (yi < fminf(y3, y4) || yi > fmaxf(y3, y4)) return false;
     if (pointIntersection)
     {
         pointIntersection->x = xi;
