d7b4b9e4857809c249b0451edff8a7b93e4a3872
raysan5 <raysan5@gmail.com>
2021-01-02 18:15:13 +0100

    Update year to 2021

--- src/gestures.h
+++ src/gestures.h
@@ -24,7 +24,7 @@
 *
 *   LICENSE: zlib/libpng
 *
-*   Copyright (c) 2014-2020 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2014-2021 Ramon Santamaria (@raysan5)
 *
 *   This software is provided "as-is", without any express or implied warranty. In no event
 *   will the authors be held liable for any damages arising from the use of this software.
--- src/rglfw.c
+++ src/rglfw.c
@@ -7,7 +7,7 @@
 *
 *   LICENSE: zlib/libpng
 *
-*   Copyright (c) 2017-2020 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2017-2021 Ramon Santamaria (@raysan5)
 *
 *   This software is provided "as-is", without any express or implied warranty. In no event
 *   will the authors be held liable for any damages arising from the use of this software.
