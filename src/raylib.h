0f309b9b16c7986c5bda65ff002b7a5b432642d4
raysan5 <raysan5@gmail.com>
2021-02-05 19:49:05 +0100

    REMOVED: MeshNormalsSmooth() #1421
    Current implementation is probably wrong and it should be reimplemented from scratch, in the meantime, I prefer to remove the function.

--- src/raylib.h
+++ src/raylib.h
@@ -1378,7 +1378,6 @@ RLAPI Mesh GenMeshCubicmap(Image cubicmap, Vector3 cubeSize);                   
 RLAPI BoundingBox MeshBoundingBox(Mesh mesh);                                                           // Compute mesh bounding box limits
 RLAPI void MeshTangents(Mesh *mesh);                                                                    // Compute mesh tangents
 RLAPI void MeshBinormals(Mesh *mesh);                                                                   // Compute mesh binormals
-RLAPI void MeshNormalsSmooth(Mesh *mesh);                                                               // Smooth (average) vertex normals
 
 // Model drawing functions
 RLAPI void DrawModel(Model model, Vector3 position, float scale, Color tint);                           // Draw a model (with texture if set)
