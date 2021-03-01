742206c1f27dc987d3bf2ca09e5e72db8b6840f8
Ray <raysan5@gmail.com>
2021-03-02 00:31:04 +0100

    Review formatting

--- src/raymath.h
+++ src/raymath.h
@@ -1271,7 +1271,8 @@ RMDEF Quaternion QuaternionSlerp(Quaternion q1, Quaternion q2, float amount)
 
     float cosHalfTheta =  q1.x*q2.x + q1.y*q2.y + q1.z*q2.z + q1.w*q2.w;
 
-    if (cosHalfTheta < 0) {
+    if (cosHalfTheta < 0) 
+    {
         q2.x = -q2.x; q2.y = -q2.y; q2.z = -q2.z; q2.w = -q2.w;
         cosHalfTheta = -cosHalfTheta;
     }    
