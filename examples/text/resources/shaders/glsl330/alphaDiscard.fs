668ba870e4e84991770a14305317ae0bfafbd777
Vlad Adrian <avalorrian@gmail.com>
2021-03-28 17:08:14 +0300

    Added draw 3d text example (#1689)

---
+++ examples/text/resources/shaders/glsl330/alphaDiscard.fs
@@ -0,0 +1,19 @@
+#version 330
+
+// Input vertex attributes (from vertex shader)
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+// Input uniform values
+uniform sampler2D texture0;
+uniform vec4 colDiffuse;
+
+// Output fragment color
+out vec4 finalColor;
+
+void main()
+{
+    vec4 texelColor = texture(texture0, fragTexCoord);
+    if (texelColor.a == 0.0) discard;
+    finalColor = texelColor * fragColor * colDiffuse;
+}
