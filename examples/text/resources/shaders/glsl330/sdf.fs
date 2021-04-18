1cb32ff8a20656ed8cccb1aa08faace0c4c178c6
Stephan Soller <stephan.soller@helionweb.de>
2021-04-18 16:31:48 +0200

    Added zoom independent sharp edges to the SDF text example.

--- examples/text/resources/shaders/glsl330/sdf.fs
+++ examples/text/resources/shaders/glsl330/sdf.fs
@@ -12,14 +12,14 @@ uniform vec4 colDiffuse;
 out vec4 finalColor;
 
 // NOTE: Add here your custom variables
-const float smoothing = 1.0/16.0;
 
 void main()
 {
     // Texel color fetching from texture sampler
     // NOTE: Calculate alpha using signed distance field (SDF)
-    float distance = texture(texture0, fragTexCoord).a;
-    float alpha = smoothstep(0.5 - smoothing, 0.5 + smoothing, distance);
+    float distanceFromOutline = texture(texture0, fragTexCoord).a - 0.5;
+    float distanceChangePerFragment = length(vec2(dFdx(distanceFromOutline), dFdy(distanceFromOutline)));
+    float alpha = smoothstep(-distanceChangePerFragment, distanceChangePerFragment, distanceFromOutline);
     
     // Calculate final fragment color
     finalColor = vec4(fragColor.rgb, fragColor.a*alpha);
