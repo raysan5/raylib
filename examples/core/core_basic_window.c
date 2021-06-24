43efe92e215901da2520a4d19e995e5a87c8e948
Ray <raysan5@gmail.com>
2021-06-24 10:39:43 +0200

    Update core_basic_window.c

--- examples/core/core_basic_window.c
+++ examples/core/core_basic_window.c
@@ -45,9 +45,9 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
