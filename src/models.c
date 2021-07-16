33b3f140733eb7a2bbd9986b7c82e80460427ed4
Jason Penick <strikebackstudios@gmail.com>
2021-07-16 16:44:32 -0500

    vboId location fix

--- src/models.c
+++ src/models.c
@@ -2738,7 +2738,7 @@ void GenMeshTangents(Mesh *mesh)
 		if (mesh->vboId[SHADER_LOC_VERTEX_TANGENT] != 0)
 		{
 			// Upate existing vertex buffer
-			rlUpdateVertexBuffer(mesh->vboId, mesh->tangents, mesh->vertexCount*4*sizeof(float), 0);
+			rlUpdateVertexBuffer(mesh->vboId[SHADER_LOC_VERTEX_TANGENT], mesh->tangents, mesh->vertexCount*4*sizeof(float), 0);
 		}
 		else
 		{
