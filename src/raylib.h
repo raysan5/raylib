fd844b014422544ec3227c96408d1f0d478025b2
Ryan Roden-Corrent <ryan@rcorre.net>
2021-08-09 16:56:21 -0400

    Document Get{Key,Char}Pressed return value.
    I think it is useful to know what the empty behavior is in cases where
    you are enumerating key presses (e.g. implementing "press any key to
    continue").

--- src/raylib.h
+++ src/raylib.h
@@ -1077,8 +1077,8 @@ RLAPI bool IsKeyDown(int key);                                // Check if a key 
 RLAPI bool IsKeyReleased(int key);                            // Check if a key has been released once
 RLAPI bool IsKeyUp(int key);                                  // Check if a key is NOT being pressed
 RLAPI void SetExitKey(int key);                               // Set a custom key to exit program (default is ESC)
-RLAPI int GetKeyPressed(void);                                // Get key pressed (keycode), call it multiple times for keys queued
-RLAPI int GetCharPressed(void);                               // Get char pressed (unicode), call it multiple times for chars queued
+RLAPI int GetKeyPressed(void);                                // Get key pressed (keycode), call it multiple times for keys queued. Returns 0 when the queue is empty.
+RLAPI int GetCharPressed(void);                               // Get char pressed (unicode), call it multiple times for chars queued. Returns 0 when the queue is empty.
 
 // Input-related functions: gamepads
 RLAPI bool IsGamepadAvailable(int gamepad);                   // Check if a gamepad is available
