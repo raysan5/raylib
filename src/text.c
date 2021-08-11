7e27c2ffa82337c6b4649295b4df1c7e1829c7fc
raysan5 <raysan5@gmail.com>
2021-08-11 18:53:06 +0200

    TODO: DrawTextPro() rotation not working?

--- src/text.c
+++ src/text.c
@@ -893,12 +893,15 @@ void DrawTextEx(Font font, const char *text, Vector2 position, float fontSize, f
 // Draw text using Font and pro parameters (rotation)
 void DrawTextPro(Font font, const char *text, Vector2 position, Vector2 origin, float rotation, float fontSize, float spacing, Color tint)
 {
+    // TODO: Not working... why???
+
     rlPushMatrix();
 
-        rlTranslatef(origin.x, origin.y, 0.0f);
+        //rlTranslatef(position.x, position.y, 0.0f);
         rlRotatef(rotation, 0.0f, 0.0f, 1.0f);
+        rlTranslatef(-origin.x, -origin.y, 0.0f);
         
-        DrawTextEx(font, text, position, fontSize, spacing, tint);
+        DrawTextEx(font, text, (Vector2){ position.x, position.y }, fontSize, spacing, tint);
     
     rlPopMatrix();
 }
