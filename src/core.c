b154bc33f55bf7b7ad3fab5841a8d49043e2e979
Ray <raysan5@gmail.com>
2021-08-22 01:08:11 +0200

    Added note on GLFW custom allocators for the future

--- src/core.c
+++ src/core.c
@@ -3583,6 +3583,17 @@ static bool InitGraphicsDevice(int width, int height)
 
 #if defined(PLATFORM_DESKTOP) || defined(PLATFORM_WEB)
     glfwSetErrorCallback(ErrorCallback);
+    /*
+    // Setup custom allocators to match raylib ones
+    const GLFWallocator allocator = {
+        .allocate = MemAlloc,
+        .deallocate = MemFree,
+        .reallocate = MemRealloc,
+        .user = NULL
+    };
+
+    glfwInitAllocator(&allocator);
+    */
 
 #if defined(__APPLE__)
     glfwInitHint(GLFW_COCOA_CHDIR_RESOURCES, GLFW_FALSE);
