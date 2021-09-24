7d52bb429d66705700b1dc8656d20b3a4083abfc
Vadim Boev <33985574+Kronka@users.noreply.github.com>
2021-09-24 10:35:02 +0300

    Add support Android build for Visual Studio

---
+++ projects/VS2019-Android/raylib_android/raylib_android/raylib_android.Packaging/ARM64/Debug/Package/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\bin\AndroidManifest.xml : \
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\AndroidManifest.xml \
---
+++ projects/VS2019-Android/raylib_android/raylib_android/raylib_android.Packaging/ARM64/Debug/Package/bin/raylib_android.Packaging-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\bin\raylib_android.Packaging-debug-unaligned.apk : \
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\bin\raylib_android.Packaging.ap_ \
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\libs\arm64-v8a\gdbserver \
+C:\raylib-master\projects\VS2019-Android\raylib_android\raylib_android\raylib_android.Packaging\ARM64\Debug\Package\libs\arm64-v8a\libmain.so \
