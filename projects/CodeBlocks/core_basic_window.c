0fc58a06f26b2dd1d1b8cd28dff4c192e1fecba4
richard <github@fantastic.me.uk>
2021-10-19 16:48:18 +0100

    update code::blocks example to support linux

--- projects/CodeBlocks/core_basic_window.c
+++ projects/CodeBlocks/core_basic_window.c
@@ -1,11 +1,7 @@
-/*******************************************************************************************-lraylib -lglfw3 -lopengl32 -lgdi32 -lopenal32 -lwinmm
-*
+/*******************************************************************************************
 *   raylib [core] example - Basic window
 *
 *   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
 *
 *   You can find all basic examples on C:\raylib\raylib\examples folder or
 *   raylib official webpage: www.raylib.com
