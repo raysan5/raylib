30797d32010247257c38f7849ae8fd67193580cf
gtrxAC <59177874+gtrxAC@users.noreply.github.com>
2022-02-10 21:51:46 +0200

    Update file names in build scripts (#2339)
    * JSON parser: Use array for function params (#2255)
    
    * Parser: follow C convention of type before name
    
    * Update file names in build scripts

--- projects/scripts/build-rpi.sh
+++ projects/scripts/build-rpi.sh
@@ -24,7 +24,7 @@ set -e
 while getopts ":hdusrcq" opt; do
     case $opt in
         h)
-            echo "Usage: ./linux-build.sh [-hdusrcqq]"
+            echo "Usage: ./build-rpi.sh [-hdusrcqq]"
             echo " -h  Show this information"
             echo " -d  Faster builds that have debug symbols, and enable warnings"
             echo " -u  Run upx* on the executable after compilation (before -r)"
@@ -39,10 +39,10 @@ while getopts ":hdusrcq" opt; do
             echo "  requires that you have upx installed and on your path, of course."
             echo ""
             echo "Examples:"
-            echo " Build a release build:                    ./linux-build.sh"
-            echo " Build a release build, full recompile:    ./linux-build.sh -c"
-            echo " Build a debug build and run:              ./linux-build.sh -d -r"
-            echo " Build in debug, run, don't print at all:  ./linux-build.sh -drqq"
+            echo " Build a release build:                    ./build-rpi.sh"
+            echo " Build a release build, full recompile:    ./build-rpi.sh -c"
+            echo " Build a debug build and run:              ./build-rpi.sh -d -r"
+            echo " Build in debug, run, don't print at all:  ./build-rpi.sh -drqq"
             exit 0
             ;;
         d)
