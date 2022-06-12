7743af853a6c7bb809c54eb6afea3dcc6fb718dd
Ray <raysan5@gmail.com>
2022-06-12 21:13:27 +0200

    Update easings.h

--- examples/others/easings.h
+++ examples/others/easings.h
@@ -90,7 +90,7 @@
     #define EASEDEF extern
 #endif
 
-#include <math.h>       // Required for: sinf(), cosf(), sqrt(), pow()
+#include <math.h>       // Required for: sinf(), cosf(), sqrtf(), powf()
 
 #ifndef PI
     #define PI 3.14159265358979323846f //Required as PI is not always defined in math.h
@@ -101,71 +101,71 @@ extern "C" {            // Prevents name mangling of functions
 #endif
 
 // Linear Easing functions
-EASEDEF float EaseLinearNone(float t, float b, float c, float d) { return (c*t/d + b); }
-EASEDEF float EaseLinearIn(float t, float b, float c, float d) { return (c*t/d + b); }
-EASEDEF float EaseLinearOut(float t, float b, float c, float d) { return (c*t/d + b); }
-EASEDEF float EaseLinearInOut(float t,float b, float c, float d) { return (c*t/d + b); }
+EASEDEF float EaseLinearNone(float t, float b, float c, float d) { return (c*t/d + b); }                            // Ease: Linear
+EASEDEF float EaseLinearIn(float t, float b, float c, float d) { return (c*t/d + b); }                              // Ease: Linear In
+EASEDEF float EaseLinearOut(float t, float b, float c, float d) { return (c*t/d + b); }                             // Ease: Linear Out
+EASEDEF float EaseLinearInOut(float t, float b, float c, float d) { return (c*t/d + b); }                           // Ease: Linear In Out
 
 // Sine Easing functions
-EASEDEF float EaseSineIn(float t, float b, float c, float d) { return (-c*cosf(t/d*(PI/2.0f)) + c + b); }
-EASEDEF float EaseSineOut(float t, float b, float c, float d) { return (c*sinf(t/d*(PI/2.0f)) + b); }
-EASEDEF float EaseSineInOut(float t, float b, float c, float d) { return (-c/2.0f*(cosf(PI*t/d) - 1.0f) + b); }
+EASEDEF float EaseSineIn(float t, float b, float c, float d) { return (-c*cosf(t/d*(PI/2.0f)) + c + b); }            // Ease: Sine In
+EASEDEF float EaseSineOut(float t, float b, float c, float d) { return (c*sinf(t/d*(PI/2.0f)) + b); }                // Ease: Sine Out
+EASEDEF float EaseSineInOut(float t, float b, float c, float d) { return (-c/2.0f*(cosf(PI*t/d) - 1.0f) + b); }      // Ease: Sine Out
 
 // Circular Easing functions
-EASEDEF float EaseCircIn(float t, float b, float c, float d) { t /= d; return (-c*(sqrtf(1.0f - t*t) - 1.0f) + b); }
-EASEDEF float EaseCircOut(float t, float b, float c, float d) { t = t/d - 1.0f; return (c*sqrtf(1.0f - t*t) + b); }
-EASEDEF float EaseCircInOut(float t, float b, float c, float d)
+EASEDEF float EaseCircIn(float t, float b, float c, float d) { t /= d; return (-c*(sqrtf(1.0f - t*t) - 1.0f) + b); } // Ease: Circular In
+EASEDEF float EaseCircOut(float t, float b, float c, float d) { t = t/d - 1.0f; return (c*sqrtf(1.0f - t*t) + b); }  // Ease: Circular Out
+EASEDEF float EaseCircInOut(float t, float b, float c, float d)                                                      // Ease: Circular In Out
 {
     if ((t/=d/2.0f) < 1.0f) return (-c/2.0f*(sqrtf(1.0f - t*t) - 1.0f) + b);
     t -= 2.0f; return (c/2.0f*(sqrtf(1.0f - t*t) + 1.0f) + b);
 }
 
 // Cubic Easing functions
-EASEDEF float EaseCubicIn(float t, float b, float c, float d) { t /= d; return (c*t*t*t + b); }
-EASEDEF float EaseCubicOut(float t, float b, float c, float d) { t = t/d - 1.0f; return (c*(t*t*t + 1.0f) + b); }
-EASEDEF float EaseCubicInOut(float t, float b, float c, float d)
+EASEDEF float EaseCubicIn(float t, float b, float c, float d) { t /= d; return (c*t*t*t + b); }                      // Ease: Cubic In
+EASEDEF float EaseCubicOut(float t, float b, float c, float d) { t = t/d - 1.0f; return (c*(t*t*t + 1.0f) + b); }    // Ease: Cubic Out
+EASEDEF float EaseCubicInOut(float t, float b, float c, float d)                                                     // Ease: Cubic In Out
 {
     if ((t/=d/2.0f) < 1.0f) return (c/2.0f*t*t*t + b);
     t -= 2.0f; return (c/2.0f*(t*t*t + 2.0f) + b);
 }
 
 // Quadratic Easing functions
-EASEDEF float EaseQuadIn(float t, float b, float c, float d) { t /= d; return (c*t*t + b); }
-EASEDEF float EaseQuadOut(float t, float b, float c, float d) { t /= d; return (-c*t*(t - 2.0f) + b); }
-EASEDEF float EaseQuadInOut(float t, float b, float c, float d)
+EASEDEF float EaseQuadIn(float t, float b, float c, float d) { t /= d; return (c*t*t + b); }                         // Ease: Quadratic In
+EASEDEF float EaseQuadOut(float t, float b, float c, float d) { t /= d; return (-c*t*(t - 2.0f) + b); }              // Ease: Quadratic Out
+EASEDEF float EaseQuadInOut(float t, float b, float c, float d)                                                      // Ease: Quadratic In Out
 {
     if ((t/=d/2) < 1) return (((c/2)*(t*t)) + b);
     return (-c/2.0f*(((t - 1.0f)*(t - 3.0f)) - 1.0f) + b);
 }
 
 // Exponential Easing functions
-EASEDEF float EaseExpoIn(float t, float b, float c, float d) { return (t == 0.0f) ? b : (c * powf(2.0f, 10.0f*(t/d - 1.0f)) + b); }
-EASEDEF float EaseExpoOut(float t, float b, float c, float d) { return (t == d) ? (b + c) : (c * (-powf(2.0f, -10.0f*t/d) + 1.0f) + b);    }
-EASEDEF float EaseExpoInOut(float t, float b, float c, float d)
+EASEDEF float EaseExpoIn(float t, float b, float c, float d) { return (t == 0.0f) ? b : (c*powf(2.0f, 10.0f*(t/d - 1.0f)) + b); }       // Ease: Exponential In
+EASEDEF float EaseExpoOut(float t, float b, float c, float d) { return (t == d) ? (b + c) : (c*(-powf(2.0f, -10.0f*t/d) + 1.0f) + b); } // Ease: Exponential Out
+EASEDEF float EaseExpoInOut(float t, float b, float c, float d)                                                                         // Ease: Exponential In Out
 {
     if (t == 0.0f) return b;
     if (t == d) return (b + c);
-    if ((t/=d/2.0f) < 1.0f) return (c/2.0f* powf(2.0f, 10.0f*(t - 1.0f)) + b);
+    if ((t/=d/2.0f) < 1.0f) return (c/2.0f*powf(2.0f, 10.0f*(t - 1.0f)) + b);
 
     return (c/2.0f*(-powf(2.0f, -10.0f*(t - 1.0f)) + 2.0f) + b);
 }
 
 // Back Easing functions
-EASEDEF float EaseBackIn(float t, float b, float c, float d)
+EASEDEF float EaseBackIn(float t, float b, float c, float d) // Ease: Back In
 {
     float s = 1.70158f;
     float postFix = t/=d;
     return (c*(postFix)*t*((s + 1.0f)*t - s) + b);
 }
 
-EASEDEF float EaseBackOut(float t, float b, float c, float d)
+EASEDEF float EaseBackOut(float t, float b, float c, float d) // Ease: Back Out
 {
     float s = 1.70158f;
     t = t/d - 1.0f;
     return (c*(t*t*((s + 1.0f)*t + s) + 1.0f) + b);
 }
 
-EASEDEF float EaseBackInOut(float t, float b, float c, float d)
+EASEDEF float EaseBackInOut(float t, float b, float c, float d) // Ease: Back In Out
 {
     float s = 1.70158f;
     if ((t/=d/2.0f) < 1.0f)
@@ -180,7 +180,7 @@ EASEDEF float EaseBackInOut(float t, float b, float c, float d)
 }
 
 // Bounce Easing functions
-EASEDEF float EaseBounceOut(float t, float b, float c, float d)
+EASEDEF float EaseBounceOut(float t, float b, float c, float d) // Ease: Bounce Out
 {
     if ((t/=d) < (1.0f/2.75f))
     {
@@ -203,15 +203,15 @@ EASEDEF float EaseBounceOut(float t, float b, float c, float d)
     }
 }
 
-EASEDEF float EaseBounceIn(float t, float b, float c, float d) { return (c - EaseBounceOut(d - t, 0.0f, c, d) + b); }
-EASEDEF float EaseBounceInOut(float t, float b, float c, float d)
+EASEDEF float EaseBounceIn(float t, float b, float c, float d) { return (c - EaseBounceOut(d - t, 0.0f, c, d) + b); } // Ease: Bounce In
+EASEDEF float EaseBounceInOut(float t, float b, float c, float d) // Ease: Bounce In Out
 {
     if (t < d/2.0f) return (EaseBounceIn(t*2.0f, 0.0f, c, d)*0.5f + b);
     else return (EaseBounceOut(t*2.0f - d, 0.0f, c, d)*0.5f + c*0.5f + b);
 }
 
 // Elastic Easing functions
-EASEDEF float EaseElasticIn(float t, float b, float c, float d)
+EASEDEF float EaseElasticIn(float t, float b, float c, float d) // Ease: Elastic In
 {
     if (t == 0.0f) return b;
     if ((t/=d) == 1.0f) return (b + c);
@@ -224,7 +224,7 @@ EASEDEF float EaseElasticIn(float t, float b, float c, float d)
     return (-(postFix*sinf((t*d-s)*(2.0f*PI)/p )) + b);
 }
 
-EASEDEF float EaseElasticOut(float t, float b, float c, float d)
+EASEDEF float EaseElasticOut(float t, float b, float c, float d) // Ease: Elastic Out
 {
     if (t == 0.0f) return b;
     if ((t/=d) == 1.0f) return (b + c);
@@ -236,7 +236,7 @@ EASEDEF float EaseElasticOut(float t, float b, float c, float d)
     return (a*powf(2.0f,-10.0f*t)*sinf((t*d-s)*(2.0f*PI)/p) + c + b);
 }
 
-EASEDEF float EaseElasticInOut(float t, float b, float c, float d)
+EASEDEF float EaseElasticInOut(float t, float b, float c, float d) // Ease: Elastic In Out
 {
     if (t == 0.0f) return b;
     if ((t/=d/2.0f) == 2.0f) return (b + c);
