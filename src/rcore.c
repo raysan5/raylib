f579a3a708c5f544cb93b6d0b5ebdaff135d97c7
Ray <raysan5@gmail.com>
2022-07-03 11:11:50 +0200

    REVIEWED: `DecompressData()`

--- src/rcore.c
+++ src/rcore.c
@@ -3290,7 +3290,7 @@ unsigned char *DecompressData(const unsigned char *compData, int compDataSize, i
 #if defined(SUPPORT_COMPRESSION_API)
     // Decompress data from a valid DEFLATE stream
     data = RL_CALLOC(MAX_DECOMPRESSION_SIZE*1024*1024, 1);
-    int length = sinflate(data, MAX_DECOMPRESSION_SIZE, compData, compDataSize);
+    int length = sinflate(data, MAX_DECOMPRESSION_SIZE*1024*1024, compData, compDataSize);
     unsigned char *temp = RL_REALLOC(data, length);
 
     if (temp != NULL) data = temp;
