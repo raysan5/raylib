d6fcc72beb781016ff75d5113b2f102d6b80fd22
Audi Nugraha <audinue@gmail.com>
2022-07-09 17:42:41 +0700

    Update raylib.h

--- src/raylib.h
+++ src/raylib.h
@@ -86,10 +86,12 @@
 // NOTE: Microsoft specifiers to tell compiler that symbols are imported/exported from a .dll
 #if defined(_WIN32)
     #if defined(BUILD_LIBTYPE_SHARED)
+        // We are building the library as a Win32 shared library (.dll)
         #if defined(__TINYC__)
-            #include <_mingw.h>
+            #define RLAPI __attribute__(dllexport)
+        #else
+            #define RLAPI __declspec(dllexport)
         #endif
-        #define RLAPI __declspec(dllexport)     // We are building the library as a Win32 shared library (.dll)
     #elif defined(USE_LIBTYPE_SHARED)
         #define RLAPI __declspec(dllimport)     // We are using the library as a Win32 shared library (.dll)
     #endif
