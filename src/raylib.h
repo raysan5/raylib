d1aabb39578790f064717715785c877dd88be3c8
Audi Nugraha <audinue@gmail.com>
2022-07-10 00:33:18 +0700

    Allow DLL creation using TCC (#2569)
    * Fix Undefined Symbol `_ftelli64`
    
    * Add files via upload
    
    * Update raylib.h
    
    * Update raylib.h

--- src/raylib.h
+++ src/raylib.h
@@ -86,6 +86,9 @@
 // NOTE: Microsoft specifiers to tell compiler that symbols are imported/exported from a .dll
 #if defined(_WIN32)
     #if defined(BUILD_LIBTYPE_SHARED)
+        #if defined(__TINYC__)
+            #define __declspec(x) __attribute__((x))
+        #endif
         #define RLAPI __declspec(dllexport)     // We are building the library as a Win32 shared library (.dll)
     #elif defined(USE_LIBTYPE_SHARED)
         #define RLAPI __declspec(dllimport)     // We are using the library as a Win32 shared library (.dll)
