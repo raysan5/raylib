75ece337ae63c9ae37717b4729303d0cf418643a
Ray <raysan5@gmail.com>
2022-08-11 12:28:21 +0200

    Revert "Update core_3d_camera_mode.c"
    This reverts commit 4f6fbaed4137be6ba41609ce9a96c11fd240953d.

--- examples/core/core_3d_camera_mode.c
+++ examples/core/core_3d_camera_mode.c
@@ -27,7 +27,7 @@ int main(void)
 
     // Define the camera to look into our 3d world
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };  // Camera position
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
