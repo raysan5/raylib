5779239dd889dd494a1c2502889464e5128f75fb
Dennis Meinen <bluepunk@gmail.com>
2022-10-05 15:43:41 +0200

    Added actual correct example file.

--- examples/textures/textures_svg_loading.c
+++ examples/textures/textures_svg_loading.c
@@ -1,15 +1,15 @@
 /*******************************************************************************************
 *
-*   raylib [textures] example - Image loading and texture creation
+*   raylib [textures] example - SVG loading and texture creation
 *
 *   NOTE: Images are loaded in CPU memory (RAM); textures are loaded in GPU memory (VRAM)
 *
-*   Example originally created with raylib 1.3, last time updated with raylib 1.3
+*   Example originally created with raylib 4.2, last time updated with raylib 4.2
 *
 *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
 *   BSD-like license that allows static linking with closed source software
 *
-*   Copyright (c) 2015-2022 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2022 Dennis Meinen (@bixxy#4258 on Discord)
 *
 ********************************************************************************************/
 
@@ -25,11 +25,11 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - image loading");
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - svg loading");
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
 
-    Image image = LoadImage("resources/raylib_logo.png");     // Loaded in CPU memory (RAM)
+    Image image = LoadImageSvg("resources/test.svg", 400, 350);     // Loaded in CPU memory (RAM)
     Texture2D texture = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
     UnloadImage(image);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
 
@@ -52,7 +52,10 @@ int main(void)
 
             DrawTexture(texture, screenWidth/2 - texture.width/2, screenHeight/2 - texture.height/2, WHITE);
 
-            DrawText("this IS a texture loaded from an image!", 300, 370, 10, GRAY);
+            //Red boarder to illustrate how the SVG is centered within the specified dimensions
+            DrawRectangleLines((screenWidth / 2 - texture.width / 2) - 1, (screenHeight / 2 - texture.height / 2) - 1, texture.width + 2, texture.height + 2, RED);
+
+            DrawText("this IS a texture loaded from an SVG file!", 300, 410, 10, GRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
