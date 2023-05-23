ed77d8d40cb6258585e384405eea562a6f7a827f
dane_madsen <dane_madsen@hotmail.com>
2023-05-23 22:20:51 +1000

    Quick rename of the example

--- examples/textures/textures_image_rotate.c
+++ examples/textures/textures_image_rotate.c
@@ -25,7 +25,7 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - texture loading and drawing");
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - texture rotation");
 
     // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
     Image image45 = LoadImage("resources/raylib_logo.png");
