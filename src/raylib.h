f45e0cf5a804c57ff2396987fd1df85ac631e962
chemguerra <online@chemaguerra.com>
2023-06-11 15:51:00 +0200

    GetTouchPointState()

--- src/raylib.h
+++ src/raylib.h
@@ -1149,6 +1149,7 @@ RLAPI int GetTouchX(void);                                    // Get touch posit
 RLAPI int GetTouchY(void);                                    // Get touch position Y for touch point 0 (relative to screen size)
 RLAPI Vector2 GetTouchPosition(int index);                    // Get touch position XY for a touch point index (relative to screen size)
 RLAPI int GetTouchPointId(int index);                         // Get touch point identifier for given index
+RLAPI bool GetTouchPointState(int index);                     // Patch for touch events in web browsers
 RLAPI int GetTouchPointCount(void);                           // Get number of touch points
 
 //------------------------------------------------------------------------------------
