bc40012ca3d719ece17f8ba28865ccb6cdf8bbcf
Ray <raysan5@gmail.com>
2023-07-10 19:15:55 +0200

    Added missing structure on standalone mode #3160

--- src/rcamera.h
+++ src/rcamera.h
@@ -76,6 +76,14 @@
         float z;                // Vector z component
     } Vector3;
 
+    // Matrix, 4x4 components, column major, OpenGL style, right-handed
+    typedef struct Matrix {
+        float m0, m4, m8, m12;  // Matrix first row (4 components)
+        float m1, m5, m9, m13;  // Matrix second row (4 components)
+        float m2, m6, m10, m14; // Matrix third row (4 components)
+        float m3, m7, m11, m15; // Matrix fourth row (4 components)
+    } Matrix;
+
     // Camera type, defines a camera position/orientation in 3d space
     typedef struct Camera3D {
         Vector3 position;       // Camera position
