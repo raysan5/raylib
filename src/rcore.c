3439f5df09f5b7d600642687b002f3d99aa91dc1
Nickolas McDonald <43690021+n77y@users.noreply.github.com>
2023-08-20 19:02:47 -0400

    Update rcore.c

--- src/rcore.c
+++ src/rcore.c
@@ -6514,10 +6514,10 @@ static void InitEvdevInput(void)
 
     // Reset keyboard key state
     for (int i = 0; i < MAX_KEYBOARD_KEYS; i++)
-	{
-		CORE.Input.Keyboard.currentKeyState[i] = 0;
-		CORE.Input.Keyboard.keyRepeatInFrame[i] = 0;
-	}
+    {
+        CORE.Input.Keyboard.currentKeyState[i] = 0;
+        CORE.Input.Keyboard.keyRepeatInFrame[i] = 0;
+    }
 
     // Open the linux directory of "/dev/input"
     directory = opendir(DEFAULT_EVDEV_PATH);
