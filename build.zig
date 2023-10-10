4669da855aa8e27c3a1cd043d0727c23e1c518aa
Purple4pur <purple4pur@gmail.com>
2023-10-10 15:25:34 +0800

    fix zig syntax errors in examples, and make it install executables correctly

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const raylib = @import("src/build.zig");
 
-// This has been tested to work with zig master branch as of commit 87de821 or May 14 2023
+// This has been tested to work with zig 0.11.0 (67709b6, Aug 4 2023)
 pub fn build(b: *std.Build) void {
     raylib.build(b);
 }
