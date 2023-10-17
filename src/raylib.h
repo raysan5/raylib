d7d04a07a28a80569f440459f2a1ca4ea2fe2497
Alexey Kutepov <reximkut@gmail.com>
2023-10-18 04:35:38 +0700

    [raudio] Implement GetMasterVolume() (#3434)
    It feels a little unfinished when you can SetMasterVolume but can't
    really Get it. So to finish the symmetry here is the GetMasterVolume
    implementation.

--- src/raylib.h
+++ src/raylib.h
@@ -1538,6 +1538,7 @@ RLAPI void InitAudioDevice(void);                                     // Initial
 RLAPI void CloseAudioDevice(void);                                    // Close the audio device and context
 RLAPI bool IsAudioDeviceReady(void);                                  // Check if audio device has been initialized successfully
 RLAPI void SetMasterVolume(float volume);                             // Set master volume (listener)
+RLAPI float GetMasterVolume(void);                                    // Get master volume (listener)
 
 // Wave/Sound loading/unloading functions
 RLAPI Wave LoadWave(const char *fileName);                            // Load wave data from file
