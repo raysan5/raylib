737cd0be7fb009bf837a00847128b00051c5f096
Ray <raysan5@gmail.com>
2023-11-10 13:27:13 +0100

    Updated for upcoming raylib 5.0

--- src/raylib.h
+++ src/raylib.h
@@ -1,6 +1,6 @@
 /**********************************************************************************************
 *
-*   raylib v4.6-dev - A simple and easy-to-use library to enjoy videogames programming (www.raylib.com)
+*   raylib v5.0 - A simple and easy-to-use library to enjoy videogames programming (www.raylib.com)
 *
 *   FEATURES:
 *       - NO external dependencies, all required libraries included with raylib
