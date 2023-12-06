ec0a0d309fe460e2aa8c4c625400f3d34d9a1a04
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2023-12-06 09:43:39 +0100

    revert

--- src/rcamera.h
+++ src/rcamera.h
@@ -447,7 +447,7 @@ void UpdateCamera(Camera *camera, int mode)
 {
     Vector2 mousePositionDelta = GetMouseDelta();
 
-    bool moveInWorldPlane = ((mode == CAMERA_THIRD_PERSON));
+    bool moveInWorldPlane = ((mode == CAMERA_FIRST_PERSON) || (mode == CAMERA_THIRD_PERSON));
     bool rotateAroundTarget = ((mode == CAMERA_THIRD_PERSON) || (mode == CAMERA_ORBITAL));
     bool lockView = ((mode == CAMERA_FREE) || (mode == CAMERA_FIRST_PERSON) || (mode == CAMERA_THIRD_PERSON) || (mode == CAMERA_ORBITAL));
     bool rotateUp = false;
