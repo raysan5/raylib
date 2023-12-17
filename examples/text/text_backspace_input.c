571a60722abf6f06868e8cbf2d68914dbcd77734
benev0 <benjaminevanownby@gmail.com>
2023-12-17 03:34:42 -0500

    cleaned up comments

--- examples/text/text_backspace_input.c
+++ examples/text/text_backspace_input.c
@@ -1,59 +1,3 @@
-/*
-    WELCOME raylib EXAMPLES CONTRIBUTOR!
-
-    This is a basic template to anyone ready to contribute with some code example for the library,
-    here there are some guidelines on how to create an example to be included in raylib
-
-    1. File naming: <module>_<description> - Lower case filename, words separated by underscore,
-       no more than 3-4 words in total to describe the example. <module> referes to the primary
-       raylib module the example is more related with (code, shapes, textures, models, shaders, raudio).
-       i.e: core_input_multitouch, shapes_lines_bezier, shaders_palette_switch
-
-    2. Follow below template structure, example info should list the module, the short description
-       and the author of the example, twitter or github info could be also provided for the author.
-       Short description should also be used on the title of the window.
-
-    3. Code should be organized by sections:[Initialization]- [Update] - [Draw] - [De-Initialization]
-       Place your code between the dotted lines for every section, please don't mix update logic with drawing
-       and remember to unload all loaded resources.
-
-    4. Code should follow raylib conventions: https://github.com/raysan5/raylib/wiki/raylib-coding-conventions
-       Try to be very organized, using line-breaks appropiately.
-
-    5. Add comments to the specific parts of code the example is focus on.
-       Don't abuse with comments, try to be clear and impersonal on the comments.
-
-    6. Try to keep the example simple, under 300 code lines if possible. Try to avoid external dependencies.
-       Try to avoid defining functions outside the main(). Example should be as self-contained as possible.
-
-    7. About external resources, they should be placed in a [resources] folder and those resources
-       should be open and free for use and distribution. Avoid propietary content.
-
-    8. Try to keep the example simple but with a creative touch.
-       Simple but beautiful examples are more appealing to users!
-
-    9. In case of additional information is required, just come to raylib Discord channel: example-contributions
-
-    10. Have fun!
-
-    The following files should be updated when adding a new example, it's planned to create some
-    script to automatize this process but not available yet.
-
-     - raylib/examples/<category>/<category>_example_name.c
-     - raylib/examples/<category>/<category>_example_name.png
-     - raylib/examples/<category>/resources/*.*
-     - raylib/examples/Makefile
-     - raylib/examples/Makefile.Web
-     - raylib/examples/README.md
-     - raylib/projects/VS2022/examples/<category>_example_name.vcxproj
-     - raylib/projects/VS2022/raylib.sln
-     - raylib.com/common/examples.js
-     - raylib.com/examples/<category>/<category>_example_name.html
-     - raylib.com/examples/<category>/<category>_example_name.data
-     - raylib.com/examples/<category>/<category>_example_name.wasm
-     - raylib.com/examples/<category>/<category>_example_name.js
-*/
-
 /*******************************************************************************************
 *
 *   raylib [core] example - Backspace input
@@ -103,22 +47,26 @@ int main(void)
         int letter = GetCharPressed();
         int key = GetKeyPressed();
 
-        if(key)
-            printf("%d, %d\n", letter, key);
-
         while (key > 0 || letter > 0)
         {
 
+            // ingest (key) backspace
+            // if posable delete one character from the string
             if (key == KEY_BACKSPACE && usedSize > 0)
             {
                 textField[usedSize - 1] = 0;
                 usedSize--;
                 key = GetKeyPressed();
             }
+
+            // ingest (key) non character
             else if (!isKeyChar(key))
             {
                 key = GetKeyPressed();
             }
+
+            // ingest (letter) and (key)
+            // if possible wright ascii character to the string
             else if ((letter > 0) && (letter >= 32) && (letter <= 125) && (usedSize < MAX_INPUT_CHARS))
             {
                 textField[usedSize] = (char) letter;
@@ -127,6 +75,8 @@ int main(void)
                 letter = GetCharPressed();
                 key = GetKeyPressed();
             }
+
+            // ingest (letter) and (key)
             else
             {
                 letter = GetCharPressed();
