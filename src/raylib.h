22cdf5de527442b2a16275e0299dbd2fad74cf26
Alex <alexandervelez7245@gmail.com>
2023-12-19 15:34:00 -0600

    Colorful Rays!
    more default colors!

--- src/raylib.h
+++ src/raylib.h
@@ -166,32 +166,151 @@
 
 // Some Basic Colors
 // NOTE: Custom raylib color palette for amazing visuals on WHITE background
-#define LIGHTGRAY  CLITERAL(Color){ 200, 200, 200, 255 }   // Light Gray
-#define GRAY       CLITERAL(Color){ 130, 130, 130, 255 }   // Gray
-#define DARKGRAY   CLITERAL(Color){ 80, 80, 80, 255 }      // Dark Gray
-#define YELLOW     CLITERAL(Color){ 253, 249, 0, 255 }     // Yellow
-#define GOLD       CLITERAL(Color){ 255, 203, 0, 255 }     // Gold
-#define ORANGE     CLITERAL(Color){ 255, 161, 0, 255 }     // Orange
-#define PINK       CLITERAL(Color){ 255, 109, 194, 255 }   // Pink
-#define RED        CLITERAL(Color){ 230, 41, 55, 255 }     // Red
-#define MAROON     CLITERAL(Color){ 190, 33, 55, 255 }     // Maroon
-#define GREEN      CLITERAL(Color){ 0, 228, 48, 255 }      // Green
-#define LIME       CLITERAL(Color){ 0, 158, 47, 255 }      // Lime
-#define DARKGREEN  CLITERAL(Color){ 0, 117, 44, 255 }      // Dark Green
-#define SKYBLUE    CLITERAL(Color){ 102, 191, 255, 255 }   // Sky Blue
-#define BLUE       CLITERAL(Color){ 0, 121, 241, 255 }     // Blue
-#define DARKBLUE   CLITERAL(Color){ 0, 82, 172, 255 }      // Dark Blue
-#define PURPLE     CLITERAL(Color){ 200, 122, 255, 255 }   // Purple
-#define VIOLET     CLITERAL(Color){ 135, 60, 190, 255 }    // Violet
-#define DARKPURPLE CLITERAL(Color){ 112, 31, 126, 255 }    // Dark Purple
-#define BEIGE      CLITERAL(Color){ 211, 176, 131, 255 }   // Beige
-#define BROWN      CLITERAL(Color){ 127, 106, 79, 255 }    // Brown
-#define DARKBROWN  CLITERAL(Color){ 76, 63, 47, 255 }      // Dark Brown
+#define INDIANRED            CLITERAL(Color){ 205, 92, 92, 255 }     \\ Indian Red
+#define LIGHTCORAL           CLITERAL(Color){ 240, 128, 128, 255 }   \\ Light Coral
+#define SALMON               CLITERAL(Color){ 250, 128, 114, 255 }   \\ Salmon
+#define DARKSALMON           CLITERAL(Color){ 233, 150, 122, 255 }   \\ Dark Salmon
+#define LIGHTSALMON          CLITERAL(Color){ 255, 160, 122, 255 }   \\ Light Salmon
+#define CRIMSON              CLITERAL(Color){ 220, 20, 60, 255 }     \\ Crimson
+#define RED                  CLITERAL(Color){ 255, 0, 0, 255 }       \\ Red
+#define FIREBRICK            CLITERAL(Color){ 178, 34, 34, 255 }     \\ Fire Brick
+#define DARKRED              CLITERAL(Color){ 139, 0, 0, 255 }       \\ Dark Red
+#define PINK                 CLITERAL(Color){ 255, 192, 203, 255 }   \\ Pink
+#define LIGHTPINK            CLITERAL(Color){ 255, 182, 193, 255 }   \\ Light Pink
+#define HOTPINK              CLITERAL(Color){ 255, 105, 180, 255 }   \\ Hot Pink
+#define DEEPPINK             CLITERAL(Color){ 255, 20, 147, 255 }    \\ Deep Pink
+#define MEDIUMVIOLETRED      CLITERAL(Color){ 199, 21, 133, 255 }    \\ Medium Violet Red
+#define PALEVIOLETRED        CLITERAL(Color){ 219, 112, 147, 255 }   \\ Pale Violet Red
+#define LIGHTSALMON          CLITERAL(Color){ 255, 160, 122, 255 }   \\ Light Salmon
+#define CORAL                CLITERAL(Color){ 255, 127, 80, 255 }    \\ Coral
+#define TOMATO               CLITERAL(Color){ 255, 99, 71, 255 }     \\ Tomato
+#define ORANGERED            CLITERAL(Color){ 255, 69, 0, 255 }      \\ Orange Red
+#define DARKORANGE           CLITERAL(Color){ 255, 140, 0, 255 }     \\ Dark Orange
+#define ORANGE               CLITERAL(Color){ 255, 165, 0, 255 }     \\ Orange
+#define GOLD                 CLITERAL(Color){ 255, 215, 0, 255 }     \\ Gold
+#define YELLOW               CLITERAL(Color){ 255, 255, 0, 255 }     \\ Yellow
+#define LIGHTYELLOW          CLITERAL(Color){ 255, 255, 224, 255 }   \\ Light Yellow
+#define LEMONCHIFFON         CLITERAL(Color){ 255, 250, 205, 255 }   \\ Lemon Chiffon
+#define LIGHTGOLDENRODYELLOW CLITERAL(Color){ 250, 250, 210, 255 }   \\ Light Goldenrod Yellow
+#define PAPAYAWHIP           CLITERAL(Color){ 255, 239, 213, 255 }   \\ Papaya Whip
+#define MOCCASIN             CLITERAL(Color){ 255, 228, 181, 255 }   \\ Moccasin
+#define PEACHPUFF            CLITERAL(Color){ 255, 218, 185, 255 }   \\ Peach Puff
+#define PALEGOLDENROD        CLITERAL(Color){ 238, 232, 170, 255 }   \\ Pale Goldenrod
+#define KHAKI                CLITERAL(Color){ 240, 230, 140, 255 }   \\ Khaki
+#define DARKKHAKI            CLITERAL(Color){ 189, 183, 107, 255 }   \\ Dark Khaki
+#define LAVENDER             CLITERAL(Color){ 230, 230, 250, 255 }   \\ Lavender
+#define THISTLE              CLITERAL(Color){ 216, 191, 216, 255 }   \\ Thistle
+#define PLUM                 CLITERAL(Color){ 221, 160, 221, 255 }   \\ Plum
+#define VIOLET               CLITERAL(Color){ 238, 130, 238, 255 }   \\ Violet
+#define ORCHID               CLITERAL(Color){ 218, 112, 214, 255 }   \\ Orchid
+#define FUCHSIA              CLITERAL(Color){ 255, 0, 255, 255 }     \\ Fuchsia
+#define MAGENTA              CLITERAL(Color){ 255, 0, 255, 255 }     \\ Magenta
+#define MEDIUMORCHID         CLITERAL(Color){ 186, 85, 211, 255 }    \\ Medium Orchid
+#define MEDIUMPURPLE         CLITERAL(Color){ 147, 112, 219, 255 }   \\ Medium Purple
+#define REBECCAPURPLE        CLITERAL(Color){ 102, 51, 153, 255 }    \\ Rebecca Purple
+#define BLUEVIOLET           CLITERAL(Color){ 138, 43, 226, 255 }    \\ Blue Violet
+#define DARKVIOLET           CLITERAL(Color){ 148, 0, 211, 255 }     \\ Dark Violet
+#define DARKORCHID           CLITERAL(Color){ 153, 50, 204, 255 }    \\ Dark Orchid
+#define DARKMAGENTA          CLITERAL(Color){ 139, 0, 139, 255 }     \\ Dark Magenta
+#define PURPLE               CLITERAL(Color){ 128, 0, 128, 255 }     \\ Purple
+#define INDIGO               CLITERAL(Color){ 75, 0, 130, 255 }      \\ Indigo
+#define SLATEBLUE            CLITERAL(Color){ 106, 90, 205, 255 }    \\ Slate Blue
+#define DARKSLATEBLUE        CLITERAL(Color){ 72, 61, 139, 255 }     \\ Dark Slate Blue
+#define MEDIUMSLATEBLUE      CLITERAL(Color){ 123, 104, 238, 255 }   \\ Medium Slate Blue
+#define GREENYELLOW          CLITERAL(Color){ 173, 255, 47, 255 }    \\ Green Yellow
+#define CHARTREUSE           CLITERAL(Color){ 127, 255, 0, 255 }     \\ Chartreuse
+#define LAWNGREEN            CLITERAL(Color){ 124, 252, 0, 255 }     \\ Lawn Green
+#define LIME                 CLITERAL(Color){ 0, 255, 0, 255 }       \\ Lime
+#define LIMEGREEN            CLITERAL(Color){ 50, 205, 50, 255 }     \\ Lime Green
+#define PALEGREEN            CLITERAL(Color){ 152, 251, 152, 255 }   \\ Pale Green
+#define LIGHTGREEN           CLITERAL(Color){ 144, 238, 144, 255 }   \\ Light Green
+#define MEDIUMSPRINGGREEN    CLITERAL(Color){ 0, 250, 154, 255 }     \\ Medium Spring Green
+#define SPRINGGREEN          CLITERAL(Color){ 0, 255, 127, 255 }     \\ Spring Green
+#define MEDIUMSEAGREEN       CLITERAL(Color){ 60, 179, 113, 255 }    \\ Medium Sea Green
+#define SEAGREEN             CLITERAL(Color){ 46, 139, 87, 255 }     \\ Sea Green
+#define FORESTGREEN          CLITERAL(Color){ 34, 139, 34, 255 }     \\ Forest Green
+#define GREEN                CLITERAL(Color){ 0, 128, 0, 255 }       \\ Green
+#define DARKGREEN            CLITERAL(Color){ 0, 100, 0, 255 }       \\ Dark Green
+#define YELLOWGREEN          CLITERAL(Color){ 154, 205, 50, 255 }    \\ Yellow Green
+#define OLIVEDRAB            CLITERAL(Color){ 107, 142, 35, 255 }    \\ Olive Drab
+#define OLIVE                CLITERAL(Color){ 128, 128, 0, 255 }     \\ Olive
+#define DARKOLIVEGREEN       CLITERAL(Color){ 85, 107, 47, 255 }     \\ Dark Olive Green
+#define MEDIUMAQUAMARINE     CLITERAL(Color){ 102, 205, 170, 255 }   \\ Medium Aquamarine
+#define DARKSEAGREEN         CLITERAL(Color){ 143, 188, 139, 255 }   \\ Dark Sea Green
+#define LIGHTSEAGREEN        CLITERAL(Color){ 32, 178, 170, 255 }    \\ Light Sea Green
+#define DARKCYAN             CLITERAL(Color){ 0, 139, 139, 255 }     \\ Dark Cyan
+#define TEAL                 CLITERAL(Color){ 0, 128, 128, 255 }     \\ Teal
+#define AQUA                 CLITERAL(Color){ 0, 255, 255, 255 }     \\ Aqua
+#define CYAN                 CLITERAL(Color){ 0, 255, 255, 255 }     \\ Cyan
+#define LIGHTCYAN            CLITERAL(Color){ 224, 255, 255, 255 }   \\ Light Cyan
+#define PALETURQUOISE        CLITERAL(Color){ 175, 238, 238, 255 }   \\ Pale Turquoise
+#define AQUAMARINE           CLITERAL(Color){ 127, 255, 212, 255 }   \\ Aquamarine
+#define TURQUOISE            CLITERAL(Color){ 64, 224, 208, 255 }    \\ Turquoise
+#define MEDIUMTURQUOISE      CLITERAL(Color){ 72, 209, 204, 255 }    \\ Medium Turquoise
+#define DARKTURQUOISE        CLITERAL(Color){ 0, 206, 209, 255 }     \\ Dark Turquoise
+#define CADETBLUE            CLITERAL(Color){ 95, 158, 160, 255 }    \\ Cadet Blue
+#define STEELBLUE            CLITERAL(Color){ 70, 130, 180, 255 }    \\ Steel Blue
+#define LIGHTSTEELBLUE       CLITERAL(Color){ 176, 196, 222, 255 }   \\ Light Steel Blue
+#define POWDERBLUE           CLITERAL(Color){ 176, 224, 230, 255 }   \\ Powder Blue
+#define LIGHTBLUE            CLITERAL(Color){ 173, 216, 230, 255 }   \\ Light Blue
+#define SKYBLUE              CLITERAL(Color){ 135, 206, 235, 255 }   \\ Sky Blue
+#define LIGHTSKYBLUE         CLITERAL(Color){ 135, 206, 250, 255 }   \\ Light Sky Blue
+#define DEEPSKYBLUE          CLITERAL(Color){ 0, 191, 255, 255 }     \\ Deep Sky Blue
+#define DODGERBLUE           CLITERAL(Color){ 30, 144, 255, 255 }    \\ Dodger Blue
+#define CORNFLOWERBLUE       CLITERAL(Color){ 100, 149, 237, 255 }   \\ Cornflower Blue
+#define MEDIUMSLATEBLUE      CLITERAL(Color){ 123, 104, 238, 255 }   \\ Medium Slate Blue
+#define ROYALBLUE            CLITERAL(Color){ 65, 105, 225, 255 }    \\ Royal Blue
+#define BLUE                 CLITERAL(Color){ 0, 0, 255, 255 }       \\ Blue
+#define MEDIUMBLUE           CLITERAL(Color){ 0, 0, 205, 255 }       \\ Medium Blue
+#define DARKBLUE             CLITERAL(Color){ 0, 0, 139, 255 }       \\ Dark Blue
+#define NAVY                 CLITERAL(Color){ 0, 0, 128, 255 }       \\ Navy
+#define MIDNIGHTBLUE         CLITERAL(Color){ 25, 25, 112, 255 }     \\ Midnight Blue
+#define CORNSILK             CLITERAL(Color){ 255, 248, 220, 255 }   \\ Cornsilk
+#define BLANCHEDALMOND       CLITERAL(Color){ 255, 235, 205, 255 }   \\ Blanched Almond
+#define BISQUE               CLITERAL(Color){ 255, 228, 196, 255 }   \\ Bisque
+#define NAVAJOWHITE          CLITERAL(Color){ 255, 222, 173, 255 }   \\ Navajo White
+#define WHEAT                CLITERAL(Color){ 245, 222, 179, 255 }   \\ Wheat
+#define BURLYWOOD            CLITERAL(Color){ 222, 184, 135, 255 }   \\ Burly Wood
+#define TAN                  CLITERAL(Color){ 210, 180, 140, 255 }   \\ Tan
+#define ROSYBROWN            CLITERAL(Color){ 188, 143, 143, 255 }   \\ Rosy Brown
+#define SANDYBROWN           CLITERAL(Color){ 244, 164, 96, 255 }    \\ Sandy Brown
+#define GOLDENROD            CLITERAL(Color){ 218, 165, 32, 255 }    \\ Goldenrod
+#define DARKGOLDENROD        CLITERAL(Color){ 184, 134, 11, 255 }    \\ Dark Goldenrod
+#define PERU                 CLITERAL(Color){ 205, 133, 63, 255 }    \\ Peru
+#define CHOCOLATE            CLITERAL(Color){ 210, 105, 30, 255 }    \\ Chocolate
+#define SADDLEBROWN          CLITERAL(Color){ 139, 69, 19, 255 }     \\ Saddle Brown
+#define SIENNA               CLITERAL(Color){ 160, 82, 45, 255 }     \\ Sienna
+#define BROWN                CLITERAL(Color){ 165, 42, 42, 255 }     \\ Brown
+#define MAROON               CLITERAL(Color){ 128, 0, 0, 255 }       \\ Maroon
+#define WHITE                CLITERAL(Color){ 255, 255, 255, 255 }   \\ White
+#define SNOW                 CLITERAL(Color){ 255, 250, 250, 255 }   \\ Snow
+#define HONEYDEW             CLITERAL(Color){ 240, 255, 240, 255 }   \\ Honey Dew
+#define MINTCREAM            CLITERAL(Color){ 245, 255, 250, 255 }   \\ Mint Cream
+#define AZURE                CLITERAL(Color){ 240, 255, 255, 255 }   \\ Azure
+#define ALICEBLUE            CLITERAL(Color){ 240, 248, 255, 255 }   \\ Alice Blue
+#define GHOSTWHITE           CLITERAL(Color){ 248, 248, 255, 255 }   \\ Ghost White
+#define WHITESMOKE           CLITERAL(Color){ 245, 245, 245, 255 }   \\ White Smoke
+#define SEASHELL             CLITERAL(Color){ 255, 245, 238, 255 }   \\ Sea Shell
+#define BEIGE                CLITERAL(Color){ 245, 245, 220, 255 }   \\ Beige
+#define OLDLACE              CLITERAL(Color){ 253, 245, 230, 255 }   \\ Old Lace
+#define FLORALWHITE          CLITERAL(Color){ 255, 250, 240, 255 }   \\ Floral White
+#define IVORY                CLITERAL(Color){ 255, 255, 240, 255 }   \\ Ivory
+#define ANTIQUEWHITE         CLITERAL(Color){ 250, 235, 215, 255 }   \\ Antique White
+#define LINEN                CLITERAL(Color){ 250, 240, 230, 255 }   \\ Linen
+#define LAVENDERBLUSH        CLITERAL(Color){ 255, 240, 245, 255 }   \\ Lavender Blush
+#define MISTYROSE            CLITERAL(Color){ 255, 228, 225, 255 }   \\ Misty Rose
+#define GAINSBORO            CLITERAL(Color){ 220, 220, 220, 255 }   \\ Gainsboro
+#define LIGHTGRAY            CLITERAL(Color){ 211, 211, 211, 255 }   \\ Light Gray
+#define SILVER               CLITERAL(Color){ 192, 192, 192, 255 }   \\ Silver
+#define DARKGRAY             CLITERAL(Color){ 169, 169, 169, 255 }   \\ Dark Gray
+#define GRAY                 CLITERAL(Color){ 128, 128, 128, 255 }   \\ Gray
+#define DIMGRAY              CLITERAL(Color){ 105, 105, 105, 255 }   \\ Dim Gray
+#define LIGHTSLATEGRAY       CLITERAL(Color){ 119, 136, 153, 255 }   \\ Light Slate Gray
+#define SLATEGRAY            CLITERAL(Color){ 112, 128, 144, 255 }   \\ Slate Gray
+#define DARKSLATEGRAY        CLITERAL(Color){ 47, 79, 79, 255 }      \\ Dark Slate Gray
+#define BLACK                CLITERAL(Color){ 0, 0, 0, 255 }         \\ Black
 
-#define WHITE      CLITERAL(Color){ 255, 255, 255, 255 }   // White
-#define BLACK      CLITERAL(Color){ 0, 0, 0, 255 }         // Black
 #define BLANK      CLITERAL(Color){ 0, 0, 0, 0 }           // Blank (Transparent)
-#define MAGENTA    CLITERAL(Color){ 255, 0, 255, 255 }     // Magenta
 #define RAYWHITE   CLITERAL(Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
 
 //----------------------------------------------------------------------------------
