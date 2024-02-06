71f9550c433d30dd1f868c0f5f89f7a69ea59f88
MrScautHD <MrScautHD5@gmail.com>
2024-02-06 19:02:21 +0100

    Update rlgl.h

--- src/rlgl.h
+++ src/rlgl.h
@@ -666,7 +666,7 @@ RLAPI void rlglInit(int width, int height);             // Initialize rlgl (buff
 RLAPI void rlglClose(void);                             // De-initialize rlgl (buffers, shaders, textures)
 RLAPI void rlLoadExtensions(void *loader);              // Load OpenGL extensions (loader function required)
 RLAPI int rlGetVersion(void);                           // Get current OpenGL version
-RLAPI GLFWglproc rlGetProcAddress(const char* procname); // Get current Proc address
+RLAPI int rlGetProcAddress(const char* procname); // Get current Proc address
 RLAPI void rlSetFramebufferWidth(int width);            // Set current framebuffer width
 RLAPI int rlGetFramebufferWidth(void);                  // Get default framebuffer width
 RLAPI void rlSetFramebufferHeight(int height);          // Set current framebuffer height
