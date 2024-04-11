8d5697ab268687b8b74aa40fc2907871fdc087cc
freakmangd <53349189+freakmangd@users.noreply.github.com>
2024-04-11 16:53:15 -0400

    fix local dependency breakage

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const raylib = @import("src/build.zig");
 
-// This has been tested to work with zig 0.11.0 and zig 0.12.0-dev.2075+f5978181e
+// This has been tested to work with zig 0.11.0 and zig 0.12.0-dev.3632+7fb5a0b18
 pub fn build(b: *std.Build) !void {
     try raylib.build(b);
 }
