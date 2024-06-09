a0a81fddee9365657015b300ed1b892db74acff5
carverdamien <carverdamien@gmail.com>
2024-06-09 13:29:09 +0200

    Make addRaylib use options.opengl_version (#4049)

--- src/build.zig
+++ src/build.zig
@@ -22,6 +22,7 @@ pub fn addRaylib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.
         .platform_drm = options.platform_drm,
         .shared = options.shared,
         .linux_display_backend = options.linux_display_backend,
+        .opengl_version = options.opengl_version,
     });
     const raylib = raylib_dep.artifact("raylib");
 
