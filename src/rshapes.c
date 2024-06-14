fca53c77996ab0b723dd4ee5235671cba9274ca7
kai-z99 <147789796+kai-z99@users.noreply.github.com>
2024-06-13 23:19:17 -0700

    optimize (#4065)

--- src/rshapes.c
+++ src/rshapes.c
@@ -2233,9 +2233,10 @@ bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, floa
     float dx = center2.x - center1.x;      // X distance between centers
     float dy = center2.y - center1.y;      // Y distance between centers
 
-    float distance = sqrtf(dx*dx + dy*dy); // Distance between centers
+    float distanceSquared = dx * dx + dy * dy; // Distance between centers squared
+    float radiusSum = radius1 + radius2;
 
-    if (distance <= (radius1 + radius2)) collision = true;
+    collision = (distanceSquared <= (radiusSum * radiusSum));
 
     return collision;
 }
