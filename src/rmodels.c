3900e9c7651066b24b11f6743626c6d4af7870eb
SuperUserNameMan <9801802+SuperUserNameMan@users.noreply.github.com>
2024-06-27 13:20:45 +0200

    try to follow guideline convention as much as possible

--- src/rmodels.c
+++ src/rmodels.c
@@ -4815,9 +4815,9 @@ static Image LoadImageFromCgltfImage(cgltf_image *cgltfImage, const char *texPat
             else
             {
                 int base64Size = (int)strlen(cgltfImage->uri + i + 1);
-                while (cgltfImage->uri[i+base64Size] == '=') base64Size--; // remove optional paddings
-                int numberOfEncodedBits = base64Size * 6 - (base64Size * 6) % 8 ; // ignore extra bits
-                int outSize = numberOfEncodedBits / 8 ; // actual encoded bytes
+                while (cgltfImage->uri[i + base64Size] == '=') base64Size--; // remove optional paddings
+                int numberOfEncodedBits = base64Size*6 - (base64Size*6) % 8 ; // ignore extra bits
+                int outSize = numberOfEncodedBits/8 ; // actual encoded bytes
                 void *data = NULL;
 
                 cgltf_options options = { 0 };
