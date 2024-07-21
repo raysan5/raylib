fc5eab5676341d0dab918ee008810049d6b50dbf
Ray <raysan5@gmail.com>
2024-07-21 10:28:01 +0200

    Update version to avoid confusions...
    ...considering that `raylib 5.5` official release could still take some time...

--- src/raylib.h
+++ src/raylib.h
@@ -84,7 +84,7 @@
 #define RAYLIB_VERSION_MAJOR 5
 #define RAYLIB_VERSION_MINOR 5
 #define RAYLIB_VERSION_PATCH 0
-#define RAYLIB_VERSION  "5.5"
+#define RAYLIB_VERSION  "5.5-dev"
 
 // Function specifiers in case library is build/used as a shared library
 // NOTE: Microsoft specifiers to tell compiler that symbols are imported/exported from a .dll
