1cc5dac693ce3fc4f287a5dc587ec973884ea694
lnc3l0t <lnk3@protonmail.com>
2024-08-06 13:44:46 +0200

    [build.zig] check if wayland-scanner is installed
    f8675981c4ca2b9b6fecf8aca792eaf18e6de419 introduced a default value
    for linux_display_backend, which makes X11 only systems fail to build.

--- src/build.zig
+++ src/build.zig
@@ -155,6 +155,13 @@ fn compileRaylib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.
                 }
 
                 if (options.linux_display_backend == .Wayland or options.linux_display_backend == .Both) {
+                    _ = b.findProgram(&.{"wayland-scanner"}, &.{}) catch {
+                        std.log.err(
+                            \\ Wayland may not be installed on the system.
+                            \\ You can switch to X11 in your `build.zig` by changing `Options.linux_display_backend`
+                        , .{});
+                        @panic("No Wayland");
+                    };
                     raylib.defineCMacro("_GLFW_WAYLAND", null);
                     raylib.linkSystemLibrary("wayland-client");
                     raylib.linkSystemLibrary("wayland-cursor");
