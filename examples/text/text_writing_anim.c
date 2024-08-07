ff9feffdfa0ff28557b889b0ea14c6c00a4c9df4
CrackedPixel <5776225+CrackedPixel@users.noreply.github.com>
2024-08-07 18:43:13 -0500

    update text writing anim

--- examples/text/text_writing_anim.c
+++ examples/text/text_writing_anim.c
@@ -52,7 +52,7 @@ int main(void)
             DrawText(TextSubtext(message, 0, framesCounter/10), 210, 160, 20, MAROON);
 
             DrawText("PRESS [ENTER] to RESTART!", 240, 260, 20, LIGHTGRAY);
-            DrawText("PRESS [SPACE] to SPEED UP!", 239, 300, 20, LIGHTGRAY);
+            DrawText("HOLD [SPACE] to SPEED UP!", 239, 300, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
