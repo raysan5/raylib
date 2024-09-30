84a4d93bc4e421e2fc654b0e4d7c513ac17990e0
Ray <raysan5@gmail.com>
2024-09-30 12:06:53 +0200

    Fix #4349

--- src/config.h
+++ src/config.h
@@ -228,7 +228,7 @@
 // rmodels: Configuration values
 //------------------------------------------------------------------------------------
 #define MAX_MATERIAL_MAPS              12       // Maximum number of shader maps supported
-#define MAX_MESH_VERTEX_BUFFERS         7       // Maximum vertex buffers (VBO) per mesh
+#define MAX_MESH_VERTEX_BUFFERS         9       // Maximum vertex buffers (VBO) per mesh
 
 //------------------------------------------------------------------------------------
 // Module: raudio - Configuration Flags
