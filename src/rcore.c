4b1aab723d0fd5425288b932ee417755cc41ca41
André Jonsson <andre.jonsson@dataductus.se>
2024-10-06 01:49:59 +0200

    Removed auto assignment to SHADER_LOC_MAP_HEIGHT loc

--- src/rcore.c
+++ src/rcore.c
@@ -1316,7 +1316,6 @@ Shader LoadShaderFromMemory(const char *vsCode, const char *fsCode)
         shader.locs[SHADER_LOC_MAP_DIFFUSE] = rlGetLocationUniform(shader.id, RL_DEFAULT_SHADER_SAMPLER2D_NAME_TEXTURE0);  // SHADER_LOC_MAP_ALBEDO
         shader.locs[SHADER_LOC_MAP_SPECULAR] = rlGetLocationUniform(shader.id, RL_DEFAULT_SHADER_SAMPLER2D_NAME_TEXTURE1); // SHADER_LOC_MAP_METALNESS
 		shader.locs[SHADER_LOC_MAP_NORMAL] = rlGetLocationUniform(shader.id, RL_DEFAULT_SHADER_SAMPLER2D_NAME_TEXTURE2);
-		shader.locs[SHADER_LOC_MAP_HEIGHT] = rlGetLocationUniform(shader.id, RL_DEFAULT_SHADER_SAMPLER2D_NAME_TEXTURE3);
 		// RL_DEFAULT_SHADER_SAMPLER2D_NAME_TEXTURE4
 	}
 
