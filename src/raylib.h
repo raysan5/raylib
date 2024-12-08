2a2acff2d5208b443f6303fc18efe8946c35be06
Rico P <RicoP@users.noreply.github.com>
2024-12-08 12:52:06 +0100

    Make sure ShaderUniformDataType matches rlShaderUniformDataType (#4577)
    * Make sure ShaderUniformDataType matches rlShaderUniformDataType
    
    * Update raylib_api.* by CI
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ray <raysan5@gmail.com>

--- src/raylib.h
+++ src/raylib.h
@@ -818,6 +818,10 @@ typedef enum {
     SHADER_UNIFORM_IVEC2,           // Shader uniform type: ivec2 (2 int)
     SHADER_UNIFORM_IVEC3,           // Shader uniform type: ivec3 (3 int)
     SHADER_UNIFORM_IVEC4,           // Shader uniform type: ivec4 (4 int)
+    SHADER_UNIFORM_UINT,            // Shader uniform type: unsigned int
+    SHADER_UNIFORM_UIVEC2,          // Shader uniform type: uivec2 (2 unsigned int)
+    SHADER_UNIFORM_UIVEC3,          // Shader uniform type: uivec3 (3 unsigned int)
+    SHADER_UNIFORM_UIVEC4,          // Shader uniform type: uivec4 (4 unsigned int)
     SHADER_UNIFORM_SAMPLER2D        // Shader uniform type: sampler2d
 } ShaderUniformDataType;
 
