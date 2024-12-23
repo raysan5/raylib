7868d600f40b80177add61e9b865f66405fd77f3
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2024-12-23 20:25:22 -0100

    [rtext] Fix default font alpha on Big Endian systems (#4624)
    * Fix rtext default font alpha on Big Endian
    
    * Endian Indepence

--- src/rtext.c
+++ src/rtext.c
@@ -247,7 +247,11 @@ extern void LoadFontDefault(void)
                 // we must consider data as little-endian order (alpha + gray)
                 ((unsigned short *)imFont.data)[i + j] = 0xffff;
             }
-            else ((unsigned short *)imFont.data)[i + j] = 0x00ff;
+            else 
+            { 
+                ((unsigned char *)imFont.data)[(i + j)*sizeof(short)] = 0xFF;
+                ((unsigned char *)imFont.data)[(i + j)*sizeof(short) + 1] = 0x00;
+            }
         }
 
         counter++;
