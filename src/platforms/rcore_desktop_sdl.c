79c29cbe247a3977ff5c5327b9b8d4f4d79ea51c
jonathandw743 <jonathandw414@gmail.com>
2025-07-06 11:36:44 +0100

    fixed compile error for PLATFORM sdl

--- src/platforms/rcore_desktop_sdl.c
+++ src/platforms/rcore_desktop_sdl.c
@@ -1612,7 +1612,7 @@ void PollInputEvents(void)
                     unsigned int codepoint = (unsigned int)SDL_StepUTF8(&event.text.text, textLen);
                     #else
                     int codepointSize = 0;
-                    codepoint = GetCodepointNextSDL(event.text.text, &codepointSize);
+                    int codepoint = GetCodepointNextSDL(event.text.text, &codepointSize);
                     #endif
                     CORE.Input.Keyboard.charPressedQueue[CORE.Input.Keyboard.charPressedQueueCount] = codepoint;
                     CORE.Input.Keyboard.charPressedQueueCount++;
