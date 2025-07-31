968385b5f4b38096c42b38eb741feddd54e8afd2
Ray <raysan5@gmail.com>
2025-07-31 23:55:46 +0200

    Update examples_template.c

--- examples/examples_template.c
+++ examples/examples_template.c
@@ -41,7 +41,7 @@
 
      - raylib/examples/<category>/<category>_example_name.c
      - raylib/examples/<category>/<category>_example_name.png
-     - raylib/examples/<category>/resources/*.*
+     - raylib/examples/<category>/resources/..
      - raylib/examples/Makefile
      - raylib/examples/Makefile.Web
      - raylib/examples/README.md
@@ -104,9 +104,11 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            // TODO: Draw everything that requires to be drawn at this point:
+            // TODO: Draw everything that requires to be drawn at this point
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);  // Example
+            DrawLineEx((Vector2){ 0, 0 }, (Vector2){ screenWidth, screenHeight }, 2.0f, RED);
+            DrawLineEx((Vector2){ 0, screenHeight }, (Vector2){ screenWidth, 0 }, 2.0f, RED);
+            DrawText("example base code template", 260, 400, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
