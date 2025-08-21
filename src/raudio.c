2deae294c691c0037a3b7da66506dd04ed39bb53
Andreas Rossberg <rossberg@mpi-sws.org>
2025-08-21 13:02:19 +0200

    [raudio] Properly close FLAC in UnloadMusicStream
    Fix  raysan5/raylib#5131.

--- src/raudio.c
+++ src/raudio.c
@@ -1767,7 +1767,7 @@ void UnloadMusicStream(Music music)
         else if (music.ctxType == MUSIC_AUDIO_QOA) qoaplay_close((qoaplay_desc *)music.ctxData);
 #endif
 #if defined(SUPPORT_FILEFORMAT_FLAC)
-        else if (music.ctxType == MUSIC_AUDIO_FLAC) drflac_free((drflac *)music.ctxData, NULL);
+        else if (music.ctxType == MUSIC_AUDIO_FLAC) { drflac_close((drflac *)music.ctxData); drflac_free((drflac *)music.ctxData, NULL); }
 #endif
 #if defined(SUPPORT_FILEFORMAT_XM)
         else if (music.ctxType == MUSIC_MODULE_XM) jar_xm_free_context((jar_xm_context_t *)music.ctxData);
