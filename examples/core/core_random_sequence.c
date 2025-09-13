161cab24da4430839414f8f4ea39ab1b8e43b44d
Ray <raysan5@gmail.com>
2025-09-13 20:57:20 +0200

    Update core_random_sequence.c

--- examples/core/core_random_sequence.c
+++ examples/core/core_random_sequence.c
@@ -16,6 +16,7 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+
 #include "raymath.h"
 
 #include <stdlib.h>     // Required for: malloc(), free()
