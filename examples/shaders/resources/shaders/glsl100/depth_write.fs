8ada37d9671682f420a2be1f1afd4b06173b81ad
Ray <raysan5@gmail.com>
2025-09-21 13:28:30 +0200

    Update depth_write.fs

--- examples/shaders/resources/shaders/glsl100/depth_write.fs
+++ examples/shaders/resources/shaders/glsl100/depth_write.fs
@@ -1,11 +1,13 @@
 #version 100
-
 #extension GL_EXT_frag_depth : enable          
+
 precision mediump float;
 
+// Input vertex attributes (from vertex shader)
 varying vec2 fragTexCoord;
 varying vec4 fragColor;
 
+// Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
