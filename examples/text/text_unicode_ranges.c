f62565dc726df092e8c136a924f3ecf9abf8b4d9
Ray <raysan5@gmail.com>
2025-10-08 18:16:46 +0200

    Update text_unicode_ranges.c

--- examples/text/text_unicode_ranges.c
+++ examples/text/text_unicode_ranges.c
@@ -6,12 +6,12 @@
 *
 *   Example originally created with raylib 5.5, last time updated with raylib 5.6
 *
-*   Example contributed by Vlad Adrian (@demizdor) and reviewed by Ramon Santamaria (@raysan5)
+*   Example contributed by Vadim Gunko (@GuvaCode) and reviewed by Ramon Santamaria (@raysan5)
 *
 *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
 *   BSD-like license that allows static linking with closed source software
 *
-*   Copyright (c) 2025 Vlad Adrian (@demizdor) and Ramon Santamaria (@raysan5)
+*   Copyright (c) 2025 Vadim Gunko (@GuvaCode) and Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
