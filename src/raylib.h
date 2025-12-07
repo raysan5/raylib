3101ab4079754dd43d7b752ed57592b546da45f7
Matorio <209939889+Matorio@users.noreply.github.com>
2025-12-06 23:35:25 -0500

    More colors
    Added "STRONGRED", and "CYAN".

--- src/raylib.h
+++ src/raylib.h
@@ -179,12 +179,14 @@
 #define ORANGE     CLITERAL(Color){ 255, 161, 0, 255 }     // Orange
 #define PINK       CLITERAL(Color){ 255, 109, 194, 255 }   // Pink
 #define RED        CLITERAL(Color){ 230, 41, 55, 255 }     // Red
+#define STRONGRED  CLITERAL(Color){ 255, 0, 0, 255}        // Strong Red
 #define MAROON     CLITERAL(Color){ 190, 33, 55, 255 }     // Maroon
 #define GREEN      CLITERAL(Color){ 0, 228, 48, 255 }      // Green
 #define LIME       CLITERAL(Color){ 0, 158, 47, 255 }      // Lime
 #define DARKGREEN  CLITERAL(Color){ 0, 117, 44, 255 }      // Dark Green
 #define SKYBLUE    CLITERAL(Color){ 102, 191, 255, 255 }   // Sky Blue
 #define BLUE       CLITERAL(Color){ 0, 121, 241, 255 }     // Blue
+#define CYAN       CLITERAL(Color){ 0, 255, 255, 255 }     // Cyan
 #define DARKBLUE   CLITERAL(Color){ 0, 82, 172, 255 }      // Dark Blue
 #define PURPLE     CLITERAL(Color){ 200, 122, 255, 255 }   // Purple
 #define VIOLET     CLITERAL(Color){ 135, 60, 190, 255 }    // Violet
