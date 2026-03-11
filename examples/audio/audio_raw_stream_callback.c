4e2c64a18722d7e07278f5a1ac244578550bdb60
dan-hoang <56205882+dan-hoang@users.noreply.github.com>
2026-03-11 16:45:53 -0700

    Remove spaces before asterisks in comments in audio_raw_stream_callback.c

--- examples/audio/audio_raw_stream_callback.c
+++ examples/audio/audio_raw_stream_callback.c
@@ -1,19 +1,19 @@
 /*******************************************************************************************
- *
- *   raylib [audio] example - raw stream callback
- *
- *   Example complexity rating: [★★★☆] 3/4
- *
- *   Example originally created with raylib 1.6, last time updated with raylib 4.2
- *
- *   Example created by Ramon Santamaria (@raysan5) and reviewed by James Hofmann (@triplefox)
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
- *   BSD-like license that allows static linking with closed source software
- *
- *   Copyright (c) 2015-2025 Ramon Santamaria (@raysan5) and James Hofmann (@triplefox)
- *
- ********************************************************************************************/
+*
+*   raylib [audio] example - raw stream callback
+*
+*   Example complexity rating: [★★★☆] 3/4
+*
+*   Example originally created with raylib 1.6, last time updated with raylib 4.2
+*
+*   Example created by Ramon Santamaria (@raysan5) and reviewed by James Hofmann (@triplefox)
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2015-2025 Ramon Santamaria (@raysan5) and James Hofmann (@triplefox)
+*
+********************************************************************************************/
 
 #include "raylib.h"
 #include <math.h>
