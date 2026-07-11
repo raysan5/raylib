1185626c50dd0c874ed39f8c6d7b188b4d057f65
Jeffery Myers <jeffm2501@gmail.com>
2026-07-11 11:41:55 -0700

    add render callback example, put the core basic window back how it was

--- examples/core/core_render_callback.c
+++ examples/core/core_render_callback.c
@@ -1,8 +1,8 @@
 /*******************************************************************************************
 *
-*   raylib [core] example - basic window
+*   raylib [core] example - basic render callback
 *
-*   Example complexity rating: [★☆☆☆] 1/4
+*   Example complexity rating: [★★☆☆] 2/4
 *
 *   Welcome to raylib!
 *
@@ -17,7 +17,7 @@
 *
 *   Enjoy using raylib. :)
 *
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
+*   Example originally created with raylib 6.1, last time updated with raylib 6.1
 *
 *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
 *   BSD-like license that allows static linking with closed source software
@@ -27,6 +27,22 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <math.h>
+
+// called by raylib once per frame. On web will use emcscrpten callback
+bool RunFrame(void)
+{
+    // return false if you want to exit
+    if (WindowShouldClose())
+        return false;
+
+    ClearBackground(BLACK);
+
+    // animate the text so you can see it move while the window is resized or dragged
+    float offset = cosf((float)GetTime() * 2.0f) *60.0f;
+    DrawTextEx(GetFontDefault(), "Hello Render Callback, drag the window!", (Vector2) { 190, 200 + offset}, 20, 2, LIGHTGRAY);
+    return true;
+}
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -38,30 +54,14 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI | FLAG_VSYNC_HINT);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic render callback");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+    // Run a game loop using the provided callback function, which will be called every frame
+    RunGameLoop(RunFrame);
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
