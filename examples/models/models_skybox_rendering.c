3d8a9729f96cd0e7f3d864501c4fd450f0c52870
Ray <raysan5@gmail.com>
2026-09-16 23:34:15 +0200

    Update models_skybox_rendering.c

--- examples/models/models_skybox_rendering.c
+++ examples/models/models_skybox_rendering.c
@@ -180,10 +180,9 @@ int main(void)
     //--------------------------------------------------------------------------------------
     UnloadShader(skybox.materials[0].shader);
     UnloadTexture(skybox.materials[0].maps[MATERIAL_MAP_CUBEMAP].texture);
-
     UnloadModel(skybox);        // Unload skybox model
+    UnloadShader(shdrCubemap);
 
-    UnloadShader(shader);
     CloseWindow();              // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
